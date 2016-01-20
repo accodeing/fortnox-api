b8569d77171f97187c89c59260e233e3015b6a6f
Hannes Elvemyr <hannes88@gmail.com>
2016-01-20 11:18:53 +0100

    Git ignores vim swap files and spec cleaned

--- spec/fortnox/api/entities/customer/validator_spec.rb
+++ spec/fortnox/api/entities/customer/validator_spec.rb
@@ -3,21 +3,19 @@ require 'fortnox/api/models/customer/validator'
 require 'fortnox/api/models/customer/entity'
 
 describe Fortnox::API::Validators::Customer do
+  let( :validator ){ Fortnox::API::Validators::Customer }
 
   describe '.validate' do
     context 'Customer with valid, simple attributes' do
-      it 'is valid' do
-        customer = Fortnox::API::Enteties::Customer.new( name: 'Test' )
-        validator = Fortnox::API::Validators::Customer
+      let( :customer ){ Fortnox::API::Enteties::Customer.new( name: 'Test' ) }
 
+      it 'is valid' do
         expect( validator.validate( customer )).to eql( true )
       end
     end
 
     context 'Customer with invalid sales_account' do
-
       let( :customer ){ Fortnox::API::Enteties::Customer.new( sales_account: 99999 )}
-      let( :validator ){ Fortnox::API::Validators::Customer }
 
       it 'is invalid' do
         expect( validator.validate( customer )).to eql( false )
