51cdd52cf9d3c0afa8b2ba1342e0e49db062b439
Hannes Elvemyr <hannes88@gmail.com>
2016-01-26 17:55:43 +0100

    Further work on Row validator

--- lib/fortnox/api/models/row/validator.rb
+++ lib/fortnox/api/models/row/validator.rb
@@ -6,6 +6,9 @@ module Fortnox
       class Row < Fortnox::API::Validators::Base
 
         attribute :account_number, inclusion: (0..9999)
+        attribute :article_number, size: 1..50
+        attribute :delivered_quantity, inclusion: (0..99999999999999)
+        attribute :description, size: 1..50
 
       end
     end
--- spec/fortnox/api/models/row/validator_spec.rb
+++ spec/fortnox/api/models/row/validator_spec.rb
@@ -7,22 +7,48 @@ describe Fortnox::API::Validators::Row do
 
   subject { described_class.new( entity ) }
 
-  describe '.validate Row with' do
+  shared_examples_for 'invalid' do |attribute, value|
+    let( :entity ){ entity_class.new( attribute => value ) }
 
+    it { is_expected.to validate_false( entity ) }
+    it { is_expected.to include_error_for( attribute, 1 ) }
+  end
+
+  shared_examples_for 'valid' do |attribute, value|
+    let( :entity ){ entity_class.new( attribute => value ) }
+
+    it { is_expected.to validate_true( entity ) }
+  end
+
+  describe '.validate Row with' do
     context 'required attributes' do
       let( :entity ){ entity_class.new }
 
       it { is_expected.to validate_true( entity ) }
     end
 
-    context 'invalid account number' do
-      let( :too_large_account_number ){ 12345 }
-      let( :entity ) do
-        entity_class.new( account_number: too_large_account_number )
-      end
+    context 'valid' do
+      it_behaves_like 'valid', :article_number, 'a' * 50
+      it_behaves_like 'valid', :article_number, 'a' * 49
+      it_behaves_like 'valid', :article_number, 'a'
+      it_behaves_like 'valid', :article_number, ''
 
-      it { is_expected.to validate_false( entity ) }
-      it { is_expected.to include_error_for( :account_number, 1 ) }
+      it_behaves_like 'valid', :delivered_quantity, 12345678901234
+
+      it_behaves_like 'valid', :description, 'a' * 50
+      it_behaves_like 'valid', :description, 'a' * 49
+      it_behaves_like 'valid', :description, 'a'
+      it_behaves_like 'valid', :description, ''
+    end
+
+    context 'invalid' do
+      it_behaves_like 'invalid', :account_number, 12345
+
+      it_behaves_like 'invalid', :article_number, 'a' * 51
+
+      it_behaves_like 'invalid', :delivered_quantity, 123456789012345
+
+      it_behaves_like 'invalid', :description, 'a' * 51
     end
   end
 end
