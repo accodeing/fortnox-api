196f8106a8e75a317983ca0775029dc6a6ab62d9
Hannes Elvemyr <hannes88@gmail.com>
2016-01-27 11:21:09 +0100

    Row validator organised by validation types
    Validator is now organised by validation types to improve readability.

--- lib/fortnox/api/models/row/validator.rb
+++ lib/fortnox/api/models/row/validator.rb
@@ -5,10 +5,11 @@ module Fortnox
     module Validators
       class Row < Fortnox::API::Validators::Base
 
-        validates :account_number, inclusion: (0..9999)
         validates :article_number, size: 1..50
-        validates :delivered_quantity, inclusion: (0..99_999_999_999_999)
         validates :description, size: 1..50
+
+        validates :account_number, inclusion: (0..9999)
+        validates :delivered_quantity, inclusion: (0..99_999_999_999_999)
         validates :discount, inclusion: (0..999_999_999_999)
         validates :house_work_hours_to_report, inclusion: (0..99_999)
         validates :price, inclusion: (0..999_999_999_999)
