4f0119f2f18ebb9893c0f1bc12172ce78c797120
Hannes Elvemyr <hannes88@gmail.com>
2016-01-27 12:06:07 +0100

    Improved RSpec output for row/validator_spec
    The RSpec output was not very clear with the former organisation of
    row/validator_spec. It read like this:
    "Fortnox::API::Validators::Row.validate Row with invalid
    house_work_hours_to_report behaves like invalid should include error
    for house_work_hours_to_report".
    
    More specific, the valid/invalid value was not included. To include it in
    outout, it must be inserted after the "behaves like invalid" since it is a
    parameter to the shared example.
    
    The updated output reads: "Fortnox::API::Validators::Row.validate Row with
    invalid attributes behaves like invalid when house_work_hours_to_report set to
    123456 should include error for house_work_hours_to_report"

--- spec/fortnox/api/models/row/validator_spec.rb
+++ spec/fortnox/api/models/row/validator_spec.rb
@@ -9,82 +9,49 @@ describe Fortnox::API::Validators::Row do
 
   shared_examples_for 'invalid' do |attribute, values|
     values.each do |value|
-      let( :entity ){ entity_class.new( attribute => value ) }
+      context "when #{attribute} set to #{value}" do
+        let( :entity ){ entity_class.new( attribute => value ) }
 
-      it{ is_expected.to validate_false( entity ) }
-      it{ is_expected.to include_error_for( attribute, 1 ) }
+        it{ is_expected.to validate_false( entity ) }
+        it{ is_expected.to include_error_for( attribute, 1 ) }
+      end
     end
   end
 
   shared_examples_for 'valid' do |attribute, values|
     values.each do |value|
-      let( :entity ){ entity_class.new( attribute => value ) }
+      context "when #{attribute} set to #{value}" do
+        let( :entity ){ entity_class.new( attribute => value ) }
 
-      it{ is_expected.to validate_true( entity ) }
+        it{ is_expected.to validate_true( entity ) }
+      end
     end
   end
 
-  describe '.validate Row with' do
-    context 'required attributes' do
+  describe '.validate Row' do
+    context 'with required attributes' do
       let( :entity ){ entity_class.new }
 
       it{ is_expected.to validate_true( entity ) }
     end
 
-    context 'valid' do
-      context 'article_number' do
-        it_behaves_like 'valid', :article_number, ['', 'a', 'a' * 49, 'a' * 50]
-      end
-
-      context 'delivered_quantity' do
-        it_behaves_like 'valid', :delivered_quantity, [0, 12345678901234]
-      end
-
-      context 'description' do
-        it_behaves_like 'valid', :description, ['', 'a', 'a' * 49, 'a' * 50]
-      end
-
-      context 'discount' do
-        it_behaves_like 'valid', :discount, [0, 123456789012]
-      end
-
-      context 'house_work_hours_to_report' do
-        it_behaves_like 'valid', :house_work_hours_to_report, [0, 12345]
-      end
-
-      context 'price' do
-        it_behaves_like 'valid', :price, [0, 123456789012]
-      end
+    context 'with valid attributes' do
+      it_behaves_like 'valid', :article_number, ['', 'a', 'a' * 49, 'a' * 50]
+      it_behaves_like 'valid', :delivered_quantity, [0, 12345678901234]
+      it_behaves_like 'valid', :description, ['', 'a', 'a' * 49, 'a' * 50]
+      it_behaves_like 'valid', :discount, [0, 123456789012]
+      it_behaves_like 'valid', :house_work_hours_to_report, [0, 12345]
+      it_behaves_like 'valid', :price, [0, 123456789012]
     end
 
-    context 'invalid' do
-      context 'account_number' do
-        it_behaves_like 'invalid', :account_number, [-1, 12345]
-      end
-
-      context 'article_number' do
-        it_behaves_like 'invalid', :article_number, ['a' * 51]
-      end
-
-      context 'delivered_quantity' do
-        it_behaves_like 'invalid', :delivered_quantity, [-1, 123456789012345]
-      end
-
-      context 'description' do
-        it_behaves_like 'invalid', :description, ['a' * 51]
-      end
-
-      context 'discount' do
-        it_behaves_like 'invalid', :discount, [-1, 1234567890123]
-      end
-
-      context 'house_work_hours_to_report' do
-        it_behaves_like 'invalid', :house_work_hours_to_report, [-1, 123456]
-      end
-
-      context 'price' do
-        it_behaves_like 'invalid', :price, [-1, 1234567890123]
-      end
+    context 'with invalid attributes' do
+      it_behaves_like 'invalid', :account_number, [-1, 12345]
+      it_behaves_like 'invalid', :article_number, ['a' * 51]
+      it_behaves_like 'invalid', :delivered_quantity, [-1, 123456789012345]
+      it_behaves_like 'invalid', :description, ['a' * 51]
+      it_behaves_like 'invalid', :discount, [-1, 1234567890123]
+      it_behaves_like 'invalid', :house_work_hours_to_report, [-1, 123456]
+      it_behaves_like 'invalid', :price, [-1, 1234567890123]
     end
   end
 end
