3d97db884bb6b09b90e7d9aa13161210486b4a8e
Hannes Elvemyr <hannes88@gmail.com>
2016-01-27 10:36:58 +0100

    All Row attr. with restrictions has validation
    Further work on row/validator.rb as well as the spec for that validator.

--- lib/fortnox/api/models/row/validator.rb
+++ lib/fortnox/api/models/row/validator.rb
@@ -7,8 +7,11 @@ module Fortnox
 
         attribute :account_number, inclusion: (0..9999)
         attribute :article_number, size: 1..50
-        attribute :delivered_quantity, inclusion: (0..99999999999999)
+        attribute :delivered_quantity, inclusion: (0..99_999_999_999_999)
         attribute :description, size: 1..50
+        attribute :discount, inclusion: (0..999_999_999_999)
+        attribute :house_work_hours_to_report, inclusion: (0..99_999)
+        attribute :price, inclusion: (0..999_999_999_999)
 
       end
     end
--- spec/fortnox/api/models/row/validator_spec.rb
+++ spec/fortnox/api/models/row/validator_spec.rb
@@ -7,17 +7,21 @@ describe Fortnox::API::Validators::Row do
 
   subject { described_class.new( entity ) }
 
-  shared_examples_for 'invalid' do |attribute, value|
-    let( :entity ){ entity_class.new( attribute => value ) }
+  shared_examples_for 'invalid' do |attribute, values|
+    values.each do |value|
+      let( :entity ){ entity_class.new( attribute => value ) }
 
-    it { is_expected.to validate_false( entity ) }
-    it { is_expected.to include_error_for( attribute, 1 ) }
+      it { is_expected.to validate_false( entity ) }
+      it { is_expected.to include_error_for( attribute, 1 ) }
+    end
   end
 
-  shared_examples_for 'valid' do |attribute, value|
-    let( :entity ){ entity_class.new( attribute => value ) }
+  shared_examples_for 'valid' do |attribute, values|
+    values.each do |value|
+      let( :entity ){ entity_class.new( attribute => value ) }
 
-    it { is_expected.to validate_true( entity ) }
+      it { is_expected.to validate_true( entity ) }
+    end
   end
 
   describe '.validate Row with' do
@@ -28,27 +32,59 @@ describe Fortnox::API::Validators::Row do
     end
 
     context 'valid' do
-      it_behaves_like 'valid', :article_number, 'a' * 50
-      it_behaves_like 'valid', :article_number, 'a' * 49
-      it_behaves_like 'valid', :article_number, 'a'
-      it_behaves_like 'valid', :article_number, ''
+      context 'article_number' do
+        it_behaves_like 'valid', :article_number, ['', 'a', 'a' * 49, 'a' * 50]
+      end
 
-      it_behaves_like 'valid', :delivered_quantity, 12345678901234
+      context 'delivered_quantity' do
+        it_behaves_like 'valid', :delivered_quantity, [0, 12345678901234]
+      end
 
-      it_behaves_like 'valid', :description, 'a' * 50
-      it_behaves_like 'valid', :description, 'a' * 49
-      it_behaves_like 'valid', :description, 'a'
-      it_behaves_like 'valid', :description, ''
+      context 'description' do
+        it_behaves_like 'valid', :description, ['', 'a', 'a' * 49, 'a' * 50]
+      end
+
+      context 'discount' do
+        it_behaves_like 'valid', :discount, [0, 123456789012]
+      end
+
+      context 'house_work_hours_to_report' do
+        it_behaves_like 'valid', :house_work_hours_to_report, [0, 12345]
+      end
+
+      context 'price' do
+        it_behaves_like 'valid', :price, [0, 123456789012]
+      end
     end
 
     context 'invalid' do
-      it_behaves_like 'invalid', :account_number, 12345
+      context 'account_number' do
+        it_behaves_like 'invalid', :account_number, [12345]
+      end
 
-      it_behaves_like 'invalid', :article_number, 'a' * 51
+      context 'article_number' do
+        it_behaves_like 'invalid', :article_number, ['a' * 51]
+      end
 
-      it_behaves_like 'invalid', :delivered_quantity, 123456789012345
+      context 'delivered_quantity' do
+        it_behaves_like 'invalid', :delivered_quantity, [123456789012345]
+      end
 
-      it_behaves_like 'invalid', :description, 'a' * 51
+      context 'description' do
+        it_behaves_like 'invalid', :description, ['a' * 51]
+      end
+
+      context 'discount' do
+        it_behaves_like 'invalid', :discount, [1234567890123]
+      end
+
+      context 'house_work_hours_to_report' do
+        it_behaves_like 'invalid', :house_work_hours_to_report, [123456]
+      end
+
+      context 'price' do
+        it_behaves_like 'invalid', :price, [1234567890123]
+      end
     end
   end
 end
