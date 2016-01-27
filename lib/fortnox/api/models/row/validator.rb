33b3f8bdfef9133bac59b7e4f2a49871862abe49
Hannes Elvemyr <hannes88@gmail.com>
2016-01-27 11:18:07 +0100

    Row validator uses validates syntax
    Since Hanami::Validations, validators can use the validates syntax
    instead of attribute. The attribute is defined in entity-files.
    Since validators only validates attributes, this syntax is preferred.

--- lib/fortnox/api/models/row/validator.rb
+++ lib/fortnox/api/models/row/validator.rb
@@ -5,13 +5,13 @@ module Fortnox
     module Validators
       class Row < Fortnox::API::Validators::Base
 
-        attribute :account_number, inclusion: (0..9999)
-        attribute :article_number, size: 1..50
-        attribute :delivered_quantity, inclusion: (0..99_999_999_999_999)
-        attribute :description, size: 1..50
-        attribute :discount, inclusion: (0..999_999_999_999)
-        attribute :house_work_hours_to_report, inclusion: (0..99_999)
-        attribute :price, inclusion: (0..999_999_999_999)
+        validates :account_number, inclusion: (0..9999)
+        validates :article_number, size: 1..50
+        validates :delivered_quantity, inclusion: (0..99_999_999_999_999)
+        validates :description, size: 1..50
+        validates :discount, inclusion: (0..999_999_999_999)
+        validates :house_work_hours_to_report, inclusion: (0..99_999)
+        validates :price, inclusion: (0..999_999_999_999)
 
       end
     end
