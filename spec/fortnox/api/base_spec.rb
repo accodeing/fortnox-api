a1ce810615aee648b456832cb52302aa3ba4a678
Hannes Elvemyr <hannes88@gmail.com>
2016-02-11 15:40:03 +0100

    Rubocop autocorrection

--- spec/fortnox/api/base_spec.rb
+++ spec/fortnox/api/base_spec.rb
@@ -5,17 +5,17 @@ describe Fortnox::API::Base do
 
   describe 'creation' do
 
-    subject{ -> { Fortnox::API::Base.new()}}
+    subject{ ->{ Fortnox::API::Base.new() } }
 
     context 'without parameters' do
-      it{ is_expected.to raise_error( ArgumentError, /base url/ )}
+      it{ is_expected.to raise_error( ArgumentError, /base url/ ) }
     end
 
     context 'with only client_secret' do
       context 'as argument' do
-        subject{ -> { Fortnox::API::Base.new( base_url: '' )}}
+        subject{ ->{ Fortnox::API::Base.new( base_url: '' ) } }
 
-        it{ is_expected.to raise_error( ArgumentError, /client secret/ )}
+        it{ is_expected.to raise_error( ArgumentError, /client secret/ ) }
       end
 
       context 'as environment variable' do
@@ -23,15 +23,15 @@ describe Fortnox::API::Base do
           stub_const('ENV', ENV.to_hash.merge('FORTNOX_API_BASE_URL' => 'xxx'))
         end
 
-        it{ is_expected.to raise_error( ArgumentError, /client secret/ )}
+        it{ is_expected.to raise_error( ArgumentError, /client secret/ ) }
       end
     end
 
     context 'with some required parameters' do
       context 'as arguments' do
-        subject{ -> { Fortnox::API::Base.new( base_url: '', client_secret: '' )}}
+        subject{ ->{ Fortnox::API::Base.new( base_url: '', client_secret: '' ) } }
 
-        it{ is_expected.to raise_error( ArgumentError, /access token/ )}
+        it{ is_expected.to raise_error( ArgumentError, /access token/ ) }
       end
 
       context 'as environment variables' do
@@ -39,7 +39,7 @@ describe Fortnox::API::Base do
           stub_const('ENV', ENV.to_hash.merge('FORTNOX_API_CLIENT_SECRET' => 'xxx', 'FORTNOX_API_ACCESS_TOKEN' => 'xxx'))
         end
 
-        it{ is_expected.to raise_error( ArgumentError, /base url/ )}
+        it{ is_expected.to raise_error( ArgumentError, /base url/ ) }
       end
     end
 
@@ -58,7 +58,7 @@ describe Fortnox::API::Base do
         headers: {
           'Access-Token' => '3f08d038-f380-4893-94a0-a08f6e60e67a',
           'Client-Secret' => 'P5K5vE3Kun',
-          'Content-Type'=>'application/json',
+          'Content-Type' => 'application/json',
           'Accept' => 'application/json',
         }
       ).to_return(
@@ -66,7 +66,7 @@ describe Fortnox::API::Base do
       )
     end
 
-    subject{ Fortnox::API::Base.new.get( '/test', { body: '' })}
+    subject{ Fortnox::API::Base.new.get( '/test', { body: '' }) }
 
     it{ is_expected.to be_nil }
   end
@@ -83,15 +83,15 @@ describe Fortnox::API::Base do
         'http://api.fortnox.se/3/test',
       ).to_return(
         status: 500,
-        body: { 'ErrorInformation' => { 'error' => 1, 'message' => 'Räkenskapsår finns inte upplagt. För att kunna skapa en faktura krävs det att det finns ett räkenskapsår' }}.to_json,
+        body: { 'ErrorInformation' => { 'error' => 1, 'message' => 'Räkenskapsår finns inte upplagt. För att kunna skapa en faktura krävs det att det finns ett räkenskapsår' } }.to_json,
         headers: { 'Content-Type' => 'application/json' },
       )
     end
 
-    subject{ -> { Fortnox::API::Base.new.post( '/test', { body: '' })}}
+    subject{ ->{ Fortnox::API::Base.new.post( '/test', { body: '' }) } }
 
-    it{ is_expected.to raise_error( Fortnox::API::RemoteServerError )}
-    it{ is_expected.to raise_error( 'Räkenskapsår finns inte upplagt. För att kunna skapa en faktura krävs det att det finns ett räkenskapsår' )}
+    it{ is_expected.to raise_error( Fortnox::API::RemoteServerError ) }
+    it{ is_expected.to raise_error( 'Räkenskapsår finns inte upplagt. För att kunna skapa en faktura krävs det att det finns ett räkenskapsår' ) }
 
     context 'with debugging enabled' do
 
@@ -101,7 +101,7 @@ describe Fortnox::API::Base do
         Fortnox::API.debugging = false
       end
 
-      it{ is_expected.to raise_error( /\<HTTParty\:\:Request\:0x/ )}
+      it{ is_expected.to raise_error( /\<HTTParty\:\:Request\:0x/ ) }
 
     end
   end
