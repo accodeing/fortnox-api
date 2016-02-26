fb9e8d445d88854c6ea0dbf4cc8862201e6da39b
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-02-26 15:37:53 +0100

    Refactors test to use it block syntax

--- spec/fortnox/api/models/attributes/currency_spec.rb
+++ spec/fortnox/api/models/attributes/currency_spec.rb
@@ -3,27 +3,34 @@ require 'fortnox/api/models/attributes/currency'
 
 describe Fortnox::API::Model::Attribute::Currency do
 
-  class TestCase
-    include Virtus.model
-    include Fortnox::API::Model::Attribute::Currency
-  end
+  let( :model ){
+    class Model
+      include Virtus.model
+      include Fortnox::API::Model::Attribute::Currency
+    end
+  }
+
+  subject{ instance.currency }
 
   describe '.new' do
-    context 'with country code' do
-      it 'ignores empty values' do
-        test_case = TestCase.new()
-        expect( test_case.currency ).to eql( nil )
-      end
+    context 'with empty country code' do
+      let( :instance ){ model.new() }
+      it{ is_expected.to eql( nil ) }
+    end
 
-      it 'upcases lower case' do
-        test_case = TestCase.new( currency: 'sek' )
-        expect( test_case.currency ).to eql( 'SEK' )
-      end
+    context 'with lowercase country code' do
+      let( :instance ){ model.new( currency: 'sek' ) }
+      it{ is_expected.to eql( 'SEK' ) }
+    end
 
-      it 'truncates to two characters' do
-        test_case = TestCase.new( currency: 'usdollar' )
-        expect( test_case.currency ).to eql( 'USD' )
-      end
+    context 'with too long country code' do
+      let( :instance ){ model.new( currency: 'usdollar' ) }
+      it{ is_expected.to eql( 'USD' ) }
+    end
+
+    context 'with too long country code' do
+      let( :instance ){ model.new( currency: 'dollaridoos' ) }
+      it{ is_expected.to eql( 'DOL' ) }
     end
   end
 
