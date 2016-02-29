93b8177e2c1d811c4185b698af2f371595e7a7ed
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-02-29 18:14:14 +0100

    Extracts dummy classes helper into the helpers module.

---
+++ spec/support/helpers.rb
@@ -0,0 +1,1 @@
+Dir[File.expand_path('spec/support/helpers/*.rb')].each{ |file| require file }
---
+++ spec/support/helpers/dummy_class_helper.rb
@@ -0,0 +1,18 @@
+module Helpers
+  def using_test_classes &block
+    around( :all ) do |example|
+      classes_before_examples = Object.constants
+
+      yield
+
+      classes_after_examples = Object.constants
+      classes_created_by_block = classes_after_examples - classes_before_examples
+
+      example.run
+
+      classes_created_by_block.each do |klass|
+        Object.send(:remove_const, klass)
+      end
+    end
+  end
+end
