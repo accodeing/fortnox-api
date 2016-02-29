684cafe42622f8721e463f69bb7e44564a6e4712
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-02-29 16:23:28 +0100

    New helper for dummy classes, WIP

--- spec/fortnox/api/validators/attributes/currency_spec.rb
+++ spec/fortnox/api/validators/attributes/currency_spec.rb
@@ -3,30 +3,42 @@ require 'fortnox/api/models/base'
 require 'fortnox/api/models/attributes/currency'
 require 'fortnox/api/validators/attributes/currency'
 
+def using_test_classes &block
+  around( :all ) do |example|
+    classes_before_examples = Object.constants
+
+    yield
+
+    classes_after_examples = Object.constants
+    classes_created_by_block = classes_after_examples - classes_before_examples
+
+    example.run
+
+    classes_created_by_block.each do |klass|
+      Object.send(:remove_const, klass)
+    end
+  end
+end
+
 describe Fortnox::API::Validator::Attribute::Currency do
 
-  before(:all) do
-    class TestModel < Fortnox::API::Model::Base
+  using_test_classes do
+    class Model < Fortnox::API::Model::Base
       include Fortnox::API::Model::Attribute::Currency
     end
 
-    class TestValidator
+    class Validator
       extend Fortnox::API::Validator::Base
       include Fortnox::API::Validator::Attribute::Currency
     end
   end
 
-  after(:all) do
-    Object.send(:remove_const, :TestModel)
-    Object.send(:remove_const, :TestValidator)
-  end
-
   describe '.validate' do
     context 'model with invalid currency attribute' do
-      let( :instance ){ TestModel.new( currency: '-_-' ) }
+      let( :instance ){ Model.new( currency: '-_-' ) }
 
       it 'is invalid' do
-        expect( TestValidator.validate( instance )).to eql( false )
+        expect( Validator.validate( instance )).to eql( false )
       end
     end
   end
