2624842d596849f1e4fb19c633320e0b5446ac73
Hannes Elvemyr <hannes88@gmail.com>
2016-03-15 14:17:57 +0100

    Readability impr. in Repository::Base::Savers

--- lib/fortnox/api/repositories/base/savers.rb
+++ lib/fortnox/api/repositories/base/savers.rb
@@ -7,15 +7,15 @@ module Fortnox
           return true if entity.saved?
 
           hash = entity_to_hash( entity )
-          json = hash.to_json
 
-          entity.new? ? save_new( json ) : update_existing( hash )
+          return save_new( hash ) if entity.new?
+          update_existing( hash )
         end
 
       private
 
-        def save_new( json )
-          post( @options.uri, { body: json } )
+        def save_new( hash )
+          post( @options.uri, { body: hash.to_json } )
         end
 
         def update_existing( hash )
