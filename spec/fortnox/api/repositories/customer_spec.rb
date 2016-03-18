bbffced3133c734876c7f26a11a71e0415d31716
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-03-18 22:35:05 +0100

    Updates specs.

--- spec/fortnox/api/repositories/customer_spec.rb
+++ spec/fortnox/api/repositories/customer_spec.rb
@@ -4,13 +4,16 @@ require 'fortnox/api/repositories/customer'
 require 'fortnox/api/repositories/examples/all'
 require 'fortnox/api/repositories/examples/find'
 require 'fortnox/api/repositories/examples/save'
+require 'fortnox/api/repositories/examples/search'
 
-describe Fortnox::API::Repository::Customer do
+describe Fortnox::API::Repository::Customer, order: :defined, integration: true do
   include_context 'environment'
+
+  include_examples '.save', :name
 
   include_examples '.all'
 
   include_examples '.find'
 
-  include_examples '.save', :name
+  include_examples '.search', :name, 'Test'
 end
--- spec/fortnox/api/repositories/invoice_spec.rb
+++ spec/fortnox/api/repositories/invoice_spec.rb
@@ -5,12 +5,12 @@ require 'fortnox/api/repositories/examples/all'
 require 'fortnox/api/repositories/examples/find'
 require 'fortnox/api/repositories/examples/save'
 
-describe Fortnox::API::Repository::Invoice do
+describe Fortnox::API::Repository::Invoice, order: :defined, integration: true do
   include_context 'environment'
+
+  include_examples '.save', :comments, { customer_number: 1 }
 
   include_examples '.all'
 
   include_examples '.find'
-
-  include_examples '.save', :comments, { customer_number: 1 }
 end
