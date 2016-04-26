1af2ec7d69a0cd6f84e93f5229665553bb3d54e0
Hannes Elvemyr <hannes88@gmail.com>
2016-04-26 11:07:10 +0200

    WIP: JSONConvertion.remove_nil_values
    First working implementation of remove_nil_values that successfully
    cleans things like { order: { order_rows: [{ price: 'nil' }] } }

--- lib/fortnox/api/repositories/base/json_convertion.rb
+++ lib/fortnox/api/repositories/base/json_convertion.rb
@@ -13,7 +13,7 @@ module Fortnox
 
         def entity_to_hash( entity, key_map, json_entity_wrapper, keys_to_filter )
           entity_hash = entity.to_hash
-          clean_entity_hash = sanitise( entity_hash, keys_to_filter )
+          clean_entity_hash = remove_nil_values( sanitise( entity_hash, keys_to_filter ) )
           entity_json_hash = convert_hash_keys_to_json_format( clean_entity_hash, key_map )
           { json_entity_wrapper => entity_json_hash }
         end
@@ -58,14 +58,51 @@ module Fortnox
           end
         end
 
-        # Removes nil values recursively
+        # Removes nil values recursively from Hash
         def remove_nil_values( hash )
           hash.each do |key, value|
-            remove_nil_values(value) if value.is_a?(Hash)
-            hash.delete(key) if value.nil?
+            if value.nil?
+              hash.delete( key )
+            else
+              clean_collection( value )
+            end
+          end
+
+          hash.delete_if{ |_, v| v.empty? if v.respond_to?( :empty? ) }
+        end
+
+        def clean_collection ( collection )
+          case collection
+          when Array
+            collection = clean_array( collection )
+          when Hash
+            collection = clean_hash( collection )
           end
         end
 
+        def clean_array( array )
+          array.each do |element|
+            if element.nil?
+              array.remove( element )
+            else
+              clean_collection( element ) if element.respond_to?( :each )
+            end
+          end
+
+          array.delete_if{ |e| e.empty? if e.respond_to?( :empty? )}
+        end
+
+        def clean_hash( hash )
+          hash.each do |key, value|
+            if value.nil?
+              hash.delete( key )
+            else
+              clean_collection( value ) if value.respond_to?( :each )
+            end
+          end
+
+          hash.delete_if{ |_, v| v.empty? if v.respond_to?( :empty )}
+        end
       end
     end
   end
