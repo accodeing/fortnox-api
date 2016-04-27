cb4af48b34a9b539c35b908b65a6d86f306f9ceb
Hannes Elvemyr <hannes88@gmail.com>
2016-04-27 09:45:32 +0200

    Repository::Order spec test response with OrderRow
    Before, the response when saving an Order were never checked. This
    commit introduces some testing to see that the response contains
    the values we are expecting. In fact, when writing these tests I
    discovered an error in the OrderRow model, which is why the spec
    is failing for the moment...

--- spec/fortnox/api/repositories/order_spec.rb
+++ spec/fortnox/api/repositories/order_spec.rb
@@ -22,22 +22,59 @@ describe Fortnox::API::Repository::Order, order: :defined, integration: true do
 
   include_examples '.only', :cancelled, 2
 
-  describe 'OrderRow' do
-    let(:model) do
-      Fortnox::API::Model::Order.new(
+  context 'when saving an Order with OrderRows' do
+    shared_examples 'OrderRow attributes' do |order_row, attribute_map|
+      describe 'response' do
+        let( :order_row_response ) do
+          entity_wrapper = described_class.new.options.json_entity_wrapper
+          response[entity_wrapper]['OrderRows'][order_row]
+        end
+
+        attribute_map.each do |attribute, value|
+          it "includes #{attribute.inspect}" do
+            expect( order_row_response[attribute] ).to eq( value )
+          end
+        end
+      end
+    end
+    let(:response) do
+      model = Fortnox::API::Model::Order.new(
         {
           customer_number: '1',
           comments: 'A great comment about something',
           order_rows: [
-            { price: 100.0, vat: '6', article_number: '1' },
-            { price: 101.5, vat: '0', article_number: '1' }]
-        })
-    end
+            { price: 100.0,
+              vat: 6,
+              article_number: '1',
+              ordered_quantity: 1 },
+            { price: 101.5,
+              vat: 0,
+              article_number: '1',
+              ordered_quantity: 2 }]
+        }
+      )
 
-    it 'should save an Order with OrderRows' do
-      VCR.use_cassette('orders/save_new_with_order_rows') do
-        described_class.new.save(model)
+      VCR.use_cassette( 'orders/save_new_with_order_rows' ) do
+        described_class.new.save( model )
       end
     end
+
+    include_examples 'OrderRow attributes',
+                     0,
+                     {
+                       'Price' => 100.0,
+                       'VAT' => 6,
+                       'ArticleNumber' => '1',
+                       'OrderedQuantity' => 1
+                     }
+
+    include_examples 'OrderRow attributes',
+                     1,
+                     {
+                       'Price' => 101.5,
+                       'VAT' => 0,
+                       'ArticleNumber' => '1',
+                       'OrderedQuantity' => 2
+                     }
   end
 end
