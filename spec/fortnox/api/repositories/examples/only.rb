4b799f6baf4612e74a453976dd58cc8fb85e3a53
Hannes Elvemyr <hannes88@gmail.com>
2016-04-28 10:07:23 +0200

    Repo example group 'only' cleaned
    Added function `repository_only` to avoid code duplication.

--- spec/fortnox/api/repositories/examples/only.rb
+++ spec/fortnox/api/repositories/examples/only.rb
@@ -1,27 +1,23 @@
-
 shared_examples_for '.only' do |matching_filter, missing_filter|
   describe '.only' do
+    def repository_only(vcr_cassette, filter)
+      repository = described_class.new
+      vcr_dir = repository.options.json_collection_wrapper.downcase
 
-    let( :repository ){ described_class.new }
-    let( :vcr_dir ){ repository.options.json_collection_wrapper.downcase }
+      VCR.use_cassette( "#{vcr_dir}/#{vcr_cassette}" ) do
+        repository.only( filter )
+      end
+    end
 
     context "with no matches" do
-      subject do
-        VCR.use_cassette( "#{vcr_dir}/filter_miss" ) do
-          repository.only( missing_filter )
-        end
-      end
+      subject{ repository_only( 'filter_miss', missing_filter ) }
 
       it{ is_expected.to be_instance_of( Array ) }
       it{ is_expected.to have(0).entries }
     end
 
     context "with one match" do
-      subject do
-        VCR.use_cassette( "#{vcr_dir}/filter_hit" ) do
-          repository.only( matching_filter )
-        end
-      end
+      subject{ repository_only( 'filter_hit', matching_filter ) }
 
       it{ is_expected.to be_instance_of( Array ) }
       it{ is_expected.to have(1).entries }
@@ -30,9 +26,7 @@ shared_examples_for '.only' do |matching_filter, missing_filter|
     context "with invalid filter" do
       subject do
         when_performing do
-          VCR.use_cassette( "#{vcr_dir}/filter_invalid" ) do
-            repository.only( 'doesntexist' )
-          end
+          repository_only( 'filter_invalid', 'doesntexist' )
         end
       end
 
