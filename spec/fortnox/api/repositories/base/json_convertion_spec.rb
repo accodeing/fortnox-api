8203c2e8d307e8cd3e560f632d7723fb25d881c4
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-08-05 16:52:32 +0200

    WIP: test for convert_hash_keys_to_json_format
    Failing test added to shows how the method is supposed to work.

--- spec/fortnox/api/repositories/base/json_convertion_spec.rb
+++ spec/fortnox/api/repositories/base/json_convertion_spec.rb
@@ -118,4 +118,64 @@ describe Fortnox::API::Repository::JSONConvertion do
       end
     end
   end
+
+  describe 'convert_hash_keys_to_json_format' do
+    let(:admin_fee) { '22' }
+    let(:comments) { 'A comments' }
+    let(:customer_number) { '1' }
+    let(:edi_info) { 'Some EDI info' }
+    let(:ocr) { '9834782497884123'}
+    let(:order_row_1_price) { '10' }
+    let(:order_row_2_price) { '30' }
+    let(:order_row_1_vat) { '2.5' }
+    let(:order_row_2_vat) { '150' }
+    let(:hash) do
+      {
+        administration_fee_vat: admin_fee,
+        comments: comments,
+        customer_number: customer_number,
+        edi_information: edi_info,
+        invoice_rows: [
+          { price: order_row_1_price, vat: order_row_1_vat },
+          { price: order_row_2_price, vat: order_row_2_vat }
+        ],
+        ocr: ocr
+      }
+    end
+
+    let(:admin_fee_json_key) { "AdministrationFeeVAT" }
+    let(:edi_info_json_key) { "EDIInformation" }
+    let(:ocr_json_key) { "OCR" }
+    let(:vat_json_key) { 'VAT' }
+    let(:key_map) do
+      {
+        administration_fee_vat: admin_fee_json_key,
+        edi_information: edi_info_json_key,
+        ocr: ocr_json_key,
+        order_rows: { vat: vat_json_key }
+        # TODO: edi_information: { "EDIGlobalLocationNumber" }
+      }
+    end
+
+    let(:entity_json_hash) do
+      described_class.convert_hash_keys_to_json_format( hash, key_map )
+    end
+
+    subject{ entity_json_hash }
+
+    it{ is_expected.to include( admin_fee_json_key => admin_fee ) }
+    it{ is_expected.to include( "Comments" => comments ) }
+    it{ is_expected.to include( "CustomerNumber" => customer_number ) }
+    it{ is_expected.to include( edi_info_json_key => edi_info ) }
+    it{ is_expected.to include( ocr_json_key => ocr ) }
+
+    describe 'nested models' do
+      subject{ entity_json_hash["InvoiceRows"] }
+      it do
+        row_1 = { 'Price' => order_row_1_price, vat_json_key => order_row_1_vat }
+        row_2 = { 'Price' => order_row_2_price, vat_json_key => order_row_2_vat }
+        is_expected.to include( [ row_1, row_2 ] )
+      end
+    end
+  end
 end
