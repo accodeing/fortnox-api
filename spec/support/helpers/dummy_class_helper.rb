0dbe07552d5e5f801dddd96ea928a6ca82a541c9
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-08-10 12:19:57 +0200

    Fixes dry-types class caching between tests.

--- spec/support/helpers/dummy_class_helper.rb
+++ spec/support/helpers/dummy_class_helper.rb
@@ -3,12 +3,16 @@ module Helpers
     around( :all ) do |example|
       classes_before_examples = Object.constants
 
+      save_dry_types_state
+
       yield
 
       classes_after_examples = Object.constants
       classes_created_by_block = classes_after_examples - classes_before_examples
 
       example.run
+
+      clean_up_dry_types
 
       classes_created_by_block.each do |klass|
         Object.send(:remove_const, klass)
@@ -17,4 +21,17 @@ module Helpers
   end
 
   alias_method :using_test_class, :using_test_classes
+
+private
+
+  def save_dry_types_state
+    @types = Dry::Types.container._container.keys
+  end
+
+  def clean_up_dry_types
+    container = Dry::Types.container._container
+    (container.keys - @types).each { |key| container.delete(key) }
+    Dry::Types.instance_variable_set('@type_map', Concurrent::Map.new)
+  end
+
 end
