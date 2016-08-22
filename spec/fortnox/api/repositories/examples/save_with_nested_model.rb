06027c5c37aa6613b4fa7539f5ffcf9223f1b6f0
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-08-22 17:36:45 +0200

    Adds failing Repository spec for nested models
    This branch is not yet ready to merge. Found a bug when saving objects
    with nested models, probably the same as described in #62. When adding
    nested models, the translation to Fortnox attribute names is not
    correct.

---
+++ spec/fortnox/api/repositories/examples/save_with_nested_model.rb
@@ -0,0 +1,22 @@
+shared_examples_for '.save with nested model' do |required_hash, nested_key, nested_hash|
+  let( :repository ){ described_class.new }
+  let( :new_hash ) do
+    required_hash.merge( nested_key => nested_hash )
+  end
+  let( :response ) do
+    VCR.use_cassette( "#{vcr_dir}/save_with_nested_model" ) do
+      model = described_class::MODEL.new( new_hash )
+      repository.save( model )
+    end
+  end
+
+  it 'does not raise any errors' do
+    expect{ response }.to_not raise_error
+  end
+
+  describe 'response' do
+    subject{ response }
+
+    it # includes correct attributes
+  end
+end
