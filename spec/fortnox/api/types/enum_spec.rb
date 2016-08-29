91efbb5992d49ed716769c4947254f36323035d1
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-08-29 09:35:30 +0200

    Enum tests refactored to shared example group
    Renamed let variable 'described_class' to 'klass'. 'described_class'
    is an RSpec variable, which is class given to RSpec at
    the beginning of the spec (in this case Fortnox::API::Types).

--- spec/fortnox/api/types/enum_spec.rb
+++ spec/fortnox/api/types/enum_spec.rb
@@ -1,49 +1,12 @@
 require 'spec_helper'
 require 'fortnox/api/types'
+require 'fortnox/api/types/examples/enum'
 
 describe Fortnox::API::Types do
-  describe 'CountryCode' do
-
-    # TODO(Hannes): This can be pulled out as examples and then run for every
-    # enum type we have. I'm just not as good at that as you are :)
-    let( :described_class ){ Fortnox::API::Types::CountryCode }
-
-    context 'created with nil' do
-      subject{ described_class[ nil ] }
-      it{ is_expected.to be_nil }
-    end
-
-    context 'created with a random member from the CountryCodes enum' do
-      let( :enum_value ){ Fortnox::API::Types::CountryCodes.values.sample }
-      subject{ described_class[ enum_value ] }
-      it{ is_expected.to eq enum_value }
-    end
-
-    context 'created with a symoblised, random member from the CountryCodes enum' do
-      let( :enum_value ){ Fortnox::API::Types::CountryCodes.values.sample }
-      let( :input ){ enum_value.to_sym }
-      subject{ described_class[ input ] }
-      it{ is_expected.to eq enum_value }
-    end
-
-    context 'created with a lower case, random member from the CountryCodes enum' do
-      let( :enum_value ){ Fortnox::API::Types::CountryCodes.values.sample }
-      let( :input ){ enum_value.downcase }
-      subject{ described_class[ input ] }
-      it{ is_expected.to eq enum_value }
-    end
-
-    context 'created with a string that starts like a random member from the CountryCodes enum' do
-      let( :enum_value ){ Fortnox::API::Types::CountryCodes.values.sample }
-      let( :input ){ enum_value.downcase + 'more string' }
-      subject{ described_class[ input ] }
-      it{ is_expected.to eq enum_value }
-    end
-
-    context 'created with invalid input' do
-      let( :input ){ 'r4nd0m' }
-      subject { -> { described_class[ input ] } }
-      it { is_expected.to raise_error(Dry::Types::ConstraintError) }
-    end
-  end
+  it_behaves_like 'enum', 'CountryCode', 'CountryCodes'
+  it_behaves_like 'enum', 'Currency', 'Currencies'
+  it_behaves_like 'enum', 'CustomerType', 'CustomerTypes'
+  # it_behaves_like 'enum', 'DiscountType', 'DiscountTypes'
+  # it_behaves_like 'enum', 'HouseWorkType', 'HouseWorkTypes'
+  # it_behaves_like 'enum', 'VATType', 'VATTypes'
 end
---
+++ spec/fortnox/api/types/examples/enum.rb
@@ -0,0 +1,43 @@
+shared_examples_for 'enum' do |name, values|
+  describe name do
+    let( :klass ){ Fortnox::API::Types.const_get(name) }
+
+    context 'created with nil' do
+      subject{ klass[ nil ] }
+      it{ is_expected.to be_nil }
+    end
+
+    context 'created with' do
+      let( :enum_value ){ Fortnox::API::Types.const_get(values).values.sample }
+
+      context "a random member from the #{name} enum" do
+        subject{ klass[ enum_value ] }
+        it{ is_expected.to eq enum_value }
+      end
+
+      context "a symoblised, random member from the #{name} enum" do
+        let( :input ){ enum_value.to_sym }
+        subject{ klass[ input ] }
+        it{ is_expected.to eq enum_value }
+      end
+
+      context "a lower case, random member from the #{name} enum" do
+        let( :input ){ enum_value.downcase }
+        subject{ klass[ input ] }
+        it{ is_expected.to eq enum_value }
+      end
+
+      context "a string that starts like a random member from the #{name} enum" do
+        let( :input ){ enum_value.downcase + 'more string' }
+        subject{ klass[ input ] }
+        it{ is_expected.to eq enum_value }
+      end
+    end
+
+    context 'created with invalid input' do
+      let( :input ){ 'r4nd0m' }
+      subject { -> { klass[ input ] } }
+      it { is_expected.to raise_error(Dry::Types::ConstraintError) }
+    end
+  end
+end
