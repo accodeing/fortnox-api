1b331f222560c2ffb2217aefab96a10ca8f81fc3
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-08-29 10:20:09 +0200

    Unnecessary specs removed
    These specs are no longer needed when we use dry types.

--- spec/fortnox/api/models/invoice_row_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-require 'fortnox/api/models/invoice_row'
-require 'fortnox/api/models/document_row_examples'
-
-describe Fortnox::API::Model::InvoiceRow do
-  # it_behaves_like 'DocumentRow Model'
-end
--- spec/fortnox/api/models/order_row_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-require 'fortnox/api/models/order_row'
-require 'fortnox/api/models/document_row_examples'
-
-describe Fortnox::API::Model::OrderRow do
-  # it_behaves_like 'DocumentRow Model'
-end
