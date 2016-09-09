e703c1a3e150266ebd63bedbb9578b03a9ee5518
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-09-09 09:34:45 +0200

    Removes unused attributes
    Attributes were replaced with types when dry-types was introduced.

--- spec/fortnox/api/models/attributes/dummy_model_context.rb
+++
@@ -1,9 +0,0 @@
-shared_context 'create dummy Model that includes described_class' do
-  using_test_class do
-    class Model
-      include Virtus.model
-    end
-
-    Model.send(:include, described_class)
-  end
-end
