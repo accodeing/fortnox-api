b12c53312e266633899739cc8229340bff77f16e
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-09-29 08:28:42 +0200

    WIP: Failing Customer model spec (sized floats)
    Sized float tests for Customer model added, but they are failing. I
    think the tests are correct, so this indicates a bug in the attribute
    declaration...

--- spec/fortnox/api/models/customer_spec.rb
+++ spec/fortnox/api/models/customer_spec.rb
@@ -8,4 +8,7 @@ describe Fortnox::API::Model::Customer, type: :model do
   subject { described_class }
 
   it{ is_expected.to require_attribute( :name, valid_hash ) }
+  it{ is_expected.to have_sized_float( :invoice_discount, 0.0, 99_999_999_999.0, valid_hash ) }
+  it{ is_expected.to have_sized_float( :invoice_administration_fee, 0.0, 99_999_999_999.0, valid_hash ) }
+  it{ is_expected.to have_sized_float( :invoice_freight, 0.0, 99_999_999_999.0, valid_hash ) }
 end
