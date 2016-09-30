d8d04e6321f2b71268ceb2b2b4aa3978777a8267
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-09-30 18:05:50 +0200

    Fixes require attr matcher after dry-struct upd
    When dry-struct was added and dry-types was updated,
    Dry::Types::SchemaKeyError were no longer available. That exception is
    not replaced with Ruby standard error KeyError.

--- spec/support/matchers/model/require_attribute_matcher.rb
+++ spec/support/matchers/model/require_attribute_matcher.rb
@@ -5,6 +5,7 @@ module Matchers
     end
 
     class RequireAttributeMatcher
+      EXCEPTION = KeyError
 
       def initialize(attribute, valid_hash)
         @attribute = attribute
@@ -32,14 +33,14 @@ module Matchers
 
           false # Fail test since expected error is not thrown
 
-        rescue Dry::StructError
+        rescue EXCEPTION
           true
         end
 
         def includes_error_message
           @klass.new(@invalid_hash)
-        rescue Dry::StructError => error
-          if error.message.include?(expected_error_message)
+        rescue EXCEPTION => error
+          if error.message == expected_error_message
             return true
           else
             @wrong_error_message = error.message
@@ -48,17 +49,17 @@ module Matchers
         end
 
         def expected_error_message
-          "#{@attribute.inspect} is missing in Hash input"
+          @attribute.to_s
         end
 
         def no_exception_failure_message
-          "Expected class to raise #{Dry::StructError} "\
+          "Expected class to raise #{EXCEPTION} "\
           "when attribute #{@attribute.inspect} is missing."
         end
 
         def wrong_error_message
-          "Expected exception to include \"#{expected_error_message}\". "\
-          "Message was \"#{@wrong_error_message}\"."
+          "Expected exception to equal #{expected_error_message.inspect}. "\
+          "Message was #{@wrong_error_message.inspect}."
         end
     end
   end
