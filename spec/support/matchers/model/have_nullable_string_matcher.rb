5e0e4088419d1294a8eb87779abec33651c296c8
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-03 17:39:44 +0200

    Fixes broken matcher have_nullable_string
    Matcher only worked when @valid_hash was an empty hash.

--- spec/support/matchers/model/have_nullable_string_matcher.rb
+++ spec/support/matchers/model/have_nullable_string_matcher.rb
@@ -30,7 +30,7 @@ module Matchers
 
         def rejects_non_string?
           non_string = 10.0
-          @klass.new( @attribute => non_string )
+          @klass.new( @valid_hash.merge( @attribute => non_string ) )
         rescue Dry::Struct::Error => error
           expected_message = "#{@invalid_value.inspect} (#{@invalid_value.class}) "\
                              "has invalid type for #{@attribute.inspect}"
