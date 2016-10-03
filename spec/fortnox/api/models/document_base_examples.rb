8ffded2287951bc3d749342381041f17062bdb47
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-03 15:34:00 +0200

    Improves test coverage for Invoice & Order models

--- spec/fortnox/api/models/document_base_examples.rb
+++ spec/fortnox/api/models/document_base_examples.rb
@@ -1,7 +1,35 @@
 require 'fortnox/api/models/context'
 
-shared_examples_for 'DocumentBase Model' do |row_class, row_attribute|
-  it{ is_expected.to require_attribute( :customer_number, { customer_number: '12345' } ) }
+shared_examples_for 'DocumentBase Model' do |row_class, row_attribute, valid_hash|
+  it{ is_expected.to require_attribute( :customer_number, valid_hash ) }
+
+  it{ is_expected.to have_sized_string( :address1, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :address2, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :city, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :comments, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :customer_name, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :delivery_address1, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :delivery_address2, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :delivery_city, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :delivery_name, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :delivery_zip_code, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :external_invoice_reference1, 80, valid_hash ) }
+  it{ is_expected.to have_sized_string( :external_invoice_reference2, 80, valid_hash ) }
+  it{ is_expected.to have_sized_string( :external_invoice_reference2, 80, valid_hash ) }
+  it{ is_expected.to have_sized_string( :our_reference, 50, valid_hash ) }
+  it{ is_expected.to have_sized_string( :phone1, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :phone2, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :remarks, 1024, valid_hash ) }
+  it{ is_expected.to have_sized_string( :your_order_number, 30, valid_hash ) }
+  it{ is_expected.to have_sized_string( :your_reference, 50, valid_hash ) }
+  it{ is_expected.to have_sized_string( :zip_code, 1024, valid_hash ) }
+
+  it{ is_expected.to have_sized_float( :freight, 0.0, 99_999_999_999.0, valid_hash ) }
+
+  it{ is_expected.to have_country_code( :country, valid_hash ) }
+  it{ is_expected.to have_country_code( :delivery_country, valid_hash ) }
+
+  it{ is_expected.to have_currency( :currency, valid_hash ) }
 
   context "when having a(n) #{row_class}" do
     it 'returns the correct object' do
--- spec/fortnox/api/models/order_spec.rb
+++ spec/fortnox/api/models/order_spec.rb
@@ -3,9 +3,12 @@ require 'fortnox/api/models/order'
 require 'fortnox/api/models/document_base_examples'
 
 describe Fortnox::API::Model::Order, type: :model do
+  valid_hash = { customer_number: '12345' }
+
   subject{ described_class }
 
   it_behaves_like 'DocumentBase Model',
                   Fortnox::API::Model::OrderRow,
-                  :order_rows
+                  :order_rows,
+                  valid_hash
 end
