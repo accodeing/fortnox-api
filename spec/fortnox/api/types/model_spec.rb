674b33701f0c04c36b248cf10471e00de7217549
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-04 08:10:46 +0200

    Adds tests to Types::Model spec

--- spec/fortnox/api/types/model_spec.rb
+++ spec/fortnox/api/types/model_spec.rb
@@ -3,33 +3,60 @@ require 'dry-struct'
 require 'fortnox/api/types/model'
 
 RSpec.describe Fortnox::API::Types::Model do
-  using_test_class do
-    module Types
-      include Dry::Types.module
+  context 'with invalid argument' do
+    using_test_classes do
+      module Types
+        include Dry::Types.module
 
-      Age = Int.constrained(gt: 18)
+        Age = Int.constrained(gt: 18)
+      end
+
+      class TypesModelUser < Fortnox::API::Types::Model
+        attribute :age, Types::Age
+      end
+
+      class DryStructUser < Dry::Struct
+        attribute :age, Types::Age
+      end
     end
 
-    class TypesModelUser < Fortnox::API::Types::Model
-      attribute :age, Types::Age
+    let(:args){ { age: 17 } }
+
+    describe 'User inheriting directly from Dry::Struct' do
+      subject{ -> { DryStructUser.new(args) } }
+
+      it{ is_expected.to raise_error(Dry::Struct::Error) }
     end
 
-    class DryStructUser < Dry::Struct
-      attribute :age, Types::Age
+    describe "User inheriting from #{described_class}" do
+      subject{ -> { TypesModelUser.new(args) } }
+
+      it{ is_expected.to raise_error(Dry::Struct::Error) }
     end
   end
 
-  let(:args){ { age: 17 } }
+  context 'when omitting optional keys' do
+    using_test_class do
+      module Types
+        include Dry::Types.module
 
-  describe 'User inheriting directly from Dry::Struct' do
-    subject{ -> { DryStructUser.new(args) } }
+        module Nullable
+          String = Types::Strict::String.optional
+        end
+      end
 
-    it{ is_expected.to raise_error(Dry::Struct::Error) }
-  end
+      class User < Fortnox::API::Types::Model
+        attribute :optional_string, Types::Nullable::String
+      end
+    end
 
-  describe "User inheriting from #{described_class}" do
-    subject{ -> { TypesModelUser.new(args) } }
+    subject{ -> { User.new() } }
 
-    it{ is_expected.to raise_error(Dry::Struct::Error) }
+    it{ is_expected.not_to raise_error }
+
+    describe 'optional attribute' do
+      subject{ User.new().optional_string }
+      it{ is_expected.to be nil }
+    end
   end
 end
