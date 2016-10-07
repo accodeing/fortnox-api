0c80145d05628227642af88af2d559ce5bb599ad
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-07 16:13:24 +0200

    JSONConversion spec improved
    Includes more tests and clearer descriptions.

--- spec/fortnox/api/mappers/base/json_conversion_spec.rb
+++ spec/fortnox/api/mappers/base/json_conversion_spec.rb
@@ -45,29 +45,25 @@ describe Fortnox::API::Mapper::JSONConversion do
       }
     end
 
-    it{ p entity_hash }
-
-    describe 'keys without mapping' do
-      specify 'is converted correctly' do
-        expect( entity_hash[:name] ).to eq 'Ford Mustang'
-      end
+    specify 'converts keys without mapping correctly' do
+      expect( entity_hash[:name] ).to eq 'Ford Mustang'
     end
 
-    describe 'keys with mapping' do
-      specify 'is converted correctly' do
-        expect( entity_hash[:vat] ).to eq 30000
-      end
-
-      specify 'is converted correctly' do
-        expect( entity_hash[:url] ).to eq 'someurl@example.com'
-      end
+    specify 'converts keys with mapping correctly' do
+      expect( entity_hash[:vat] ).to eq 30000
     end
 
-    describe 'nested models' do
+    specify 'converts keys starting with "@" correctly'
+    # do
+    # expect( entity_hash[:url] ).to eq 'someurl@example.com'
+    # end
+
+    context 'with nested models' do
       let( :expected_nested_model_hash ) do
         [{ name: 'car', id: '1' }, { name: 'fast car', id: '2'}]
       end
-      specify 'is converted correctly' do
+
+      specify 'are converted correctly' do
         expect( entity_hash[:categories] ).to eq( expected_nested_model_hash )
       end
     end
@@ -87,7 +83,33 @@ describe Fortnox::API::Mapper::JSONConversion do
                          categories: [category1, category2] )
     end
     let( :keys_to_filter ){ [:url] }
+    let( :returned_hash ) { mapper.entity_to_hash( product, keys_to_filter ) }
+    let( :inner_hash ){ returned_hash['Product'] }
 
-    it{ p mapper.entity_to_hash( product, keys_to_filter ) }
+    it 'includes JSON entity wrapper' do
+      expect( returned_hash ).to have_key( 'Product' )
+    end
+
+    context 'with keys without mapping' do
+      specify 'converts correctly' do
+        expect( inner_hash['Name'] ).to eq( 'Ford Mustang' )
+      end
+    end
+
+    context 'with keys to filter' do
+      specify 'filteres out those keys' do
+        expect( inner_hash ).not_to have_key( '@url' )
+      end
+    end
+
+    context 'with nested models' do
+      let( :expected_nested_model_hash ) do
+        [{ 'Name': 'Cars', 'ID': '1' }, { 'Name': 'Fast Cars', 'ID': '2' }]
+      end
+
+      specify 'are converted correctly' do
+        expect( inner_hash['Categories'] ).to eq( expected_nested_model_hash )
+      end
+    end
   end
 end
