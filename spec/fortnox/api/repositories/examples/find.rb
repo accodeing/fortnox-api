8111a8fe8948ff54d5d6f0c6af6cefe071841242
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-11 09:19:58 +0200

    Adds missing describe wrappers in repository ex.
    If shared example group are missing some wrapper, it will be inserted
    into current context and before- and let-statements will conflict.

--- spec/fortnox/api/repositories/examples/find.rb
+++ spec/fortnox/api/repositories/examples/find.rb
@@ -1,29 +1,31 @@
 shared_examples_for '.find' do
-  let( :find_id ){ 1 }
-  let( :find_id_1 ) do
-    VCR.use_cassette( "#{ vcr_dir( repository ) }/find_id_1" ){ subject.find( find_id ) }
-  end
-
-  it{ p repository }
-
   describe '.find' do
-    specify 'returns correct class' do
-      expect( find_id_1.class ).to be described_class::MODEL
+    let( :find_id ){ 1 }
+    let( :find_id_1 ) do
+      VCR.use_cassette( "#{ vcr_dir( repository ) }/find_id_1" ){ subject.find( find_id ) }
     end
 
-    specify 'returns correct Customer' do
-      id_attribute = described_class::MAPPER.new.send(
-        :convert_key_from_json, described_class::UNIQUE_ID
-      )
-      expect( find_id_1.send(id_attribute).to_i ).to eq( find_id )
-    end
+    it{ p repository }
 
-    specify 'returned Customer is marked as saved' do
-      expect( find_id_1 ).to be_saved
-    end
+    describe '.find' do
+      specify 'returns correct class' do
+        expect( find_id_1.class ).to be described_class::MODEL
+      end
 
-    specify 'returned Customer is not markes as new' do
-      expect( find_id_1 ).not_to be_new
+      specify 'returns correct Customer' do
+        id_attribute = described_class::MAPPER.new.send(
+          :convert_key_from_json, described_class::UNIQUE_ID
+        )
+        expect( find_id_1.send(id_attribute).to_i ).to eq( find_id )
+      end
+
+      specify 'returned Customer is marked as saved' do
+        expect( find_id_1 ).to be_saved
+      end
+
+      specify 'returned Customer is not markes as new' do
+        expect( find_id_1 ).not_to be_new
+      end
     end
   end
 end
