d2133519f8cc891741e210751fe395b0660fa0cd
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-11 14:20:07 +0200

    Moves Mapper::Hash to Mapper::Base::Hash

--- lib/fortnox/api/mappers.rb
+++ lib/fortnox/api/mappers.rb
@@ -18,22 +18,12 @@ module Fortnox
         "[#{pairs.join(',')}]"
       end
 
-      Hash = ->( value ) do
-        pairs = value.inject([]) do |pairs,(k,v)|
-          name = Fortnox::API::Mapper::Base.canonical_name_sym( v )
-          value = Fortnox::API::Registry[ name ].call( v )
-          pairs << "\"#{ k }\":#{ value }"
-        end
-        "{#{pairs.join(',')}}"
-      end
-
       Registry.register( :fixnum, Fortnox::API::Mapper::Identity )
       Registry.register( :int, Fortnox::API::Mapper::Identity )
       Registry.register( :float, Fortnox::API::Mapper::Identity )
       Registry.register( :string, Fortnox::API::Mapper::String )
       Registry.register( :boolean, Fortnox::API::Mapper::Boolean )
       Registry.register( :array, Fortnox::API::Mapper::Array )
-      Registry.register( :hash, Fortnox::API::Mapper::Hash )
     end
   end
 end
--- lib/fortnox/api/mappers/base.rb
+++ lib/fortnox/api/mappers/base.rb
@@ -6,6 +6,17 @@ module Fortnox
       class Base
         include JSONConversion
 
+        Hash = ->( value ) do
+          pairs = value.inject([]) do |pairs,(k,v)|
+            name = Fortnox::API::Mapper::Base.canonical_name_sym( v )
+            value = Fortnox::API::Registry[ name ].call( v )
+            pairs << "\"#{ k }\":#{ value }"
+          end
+          "{#{pairs.join(',')}}"
+        end
+
+        Registry.register( :hash, Fortnox::API::Mapper::Base::Hash )
+
         def self.canonical_name_sym( value = nil )
           klass = value ? value.class : self
           klass.name.split('::').last.downcase.to_sym
