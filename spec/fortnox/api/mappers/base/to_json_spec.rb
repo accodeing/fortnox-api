df8fe048856131d7f6579c433f5452543d8c5853
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-12 11:58:40 +0200

    Adds spec for ToJSON
    This spec should probably share a context with JSONConversion...

---
+++ spec/fortnox/api/mappers/base/to_json_spec.rb
@@ -0,0 +1,113 @@
+require 'spec_helper'
+require 'fortnox/api'
+require 'fortnox/api/mappers'
+require 'fortnox/api/mappers/base/to_json'
+
+describe Fortnox::API::Mapper::ToJSON do
+  before(:all) do
+    module Test
+      class CategoryMapper < Fortnox::API::Mapper::Base
+      end
+      class ProductDesignerMapper < Fortnox::API::Mapper::Base
+      end
+      class ProductMapper < Fortnox::API::Mapper::Base
+      end
+    end
+
+    Fortnox::API::Registry.register( :category, Test::CategoryMapper )
+    Fortnox::API::Registry.register( :categories, Test::CategoryMapper )
+    Fortnox::API::Registry.register( :designer, Test::ProductDesignerMapper )
+    Fortnox::API::Registry.register( :product, Test::ProductMapper )
+  end
+
+  before do
+    module Test
+      class CategoryMapper < Fortnox::API::Mapper::Base
+        KEY_MAP = { id: 'ID' }.freeze
+      end
+
+      class ProductDesignerMapper < Fortnox::API::Mapper::Base
+        KEY_MAP = { id: 'ID' }.freeze
+      end
+
+      class ProductMapper < Fortnox::API::Mapper::Base
+        KEY_MAP = {
+          vat: 'VAT',
+          url: '@url' # TODO: How to handle url attribute?
+        }.freeze
+        JSON_ENTITY_WRAPPER = 'Product'.freeze
+        JSON_COLLECTION_WRAPPER = 'Products'.freeze
+      end
+
+      class Category < Dry::Struct
+        attribute :name, 'strict.string'
+        attribute :id, 'strict.string'
+      end
+
+      class ProductDesigner < Dry::Struct
+        attribute :name, 'strict.string'
+        attribute :id, 'strict.string'
+      end
+
+      class Product < Dry::Struct
+        attribute :url, 'strict.string'
+        attribute :name, 'strict.string'
+        attribute :vat, 'strict.float'
+        attribute :categories, Dry::Types['coercible.array'].member( Test::Category )
+        attribute :designer, Test::ProductDesigner
+      end
+    end
+  end
+
+  let( :mapper ){ Test::ProductMapper.new }
+
+  describe 'entity_to_hash' do
+    let( :category1 ){ Test::Category.new( name: 'Cars', id: '1' ) }
+    let( :category2 ){ Test::Category.new( name: 'Fast Cars', id: '2' ) }
+    let( :product_designer ){ Test::ProductDesigner.new( name: 'John Najjar', id: '23' ) }
+    let( :product ) do
+      Test::Product.new( url: 'someurl@example.com',
+                         name: 'Ford Mustang',
+                         vat: 30000.0 ,
+                         categories: [category1, category2],
+                         designer: product_designer )
+    end
+    let( :keys_to_filter ){ [:url] }
+    let( :returned_hash ){ mapper.entity_to_hash( product, keys_to_filter ) }
+    let( :inner_hash ){ returned_hash['Product'] }
+
+    it 'includes JSON entity wrapper' do
+      expect( returned_hash ).to have_key( 'Product' )
+    end
+
+    context 'with keys without mapping' do
+      specify 'converts correctly' do
+        expect( inner_hash['Name'] ).to eq( '"Ford Mustang"' )
+      end
+    end
+
+    context 'with keys to filter' do
+      specify 'filteres out those keys' do
+        expect( inner_hash ).not_to have_key( '@url' )
+      end
+    end
+
+    context 'with nested models' do
+      let( :expected_nested_model_hash ) do
+        [{ 'Name' => '"Cars"', 'ID' => '"1"' }, { 'Name' => '"Fast Cars"', 'ID' => '"2"' }]
+      end
+
+      specify 'are converted correctly' do
+        expect( inner_hash['Categories'] ).to eq( expected_nested_model_hash )
+      end
+    end
+
+    context 'with nested model' do
+      let( :expected_nested_model_hash ){ { 'Name' => '"John Najjar"', 'ID' => '"23"' } }
+
+      specify 'is converted correctly' do
+        expect( inner_hash['Designer'] ).to eq( expected_nested_model_hash )
+      end
+    end
+  end
+end
