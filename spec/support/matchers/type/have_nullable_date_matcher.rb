b5401689aac7732ecfc0b4def884b698c7be2f25
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-27 15:58:40 +0200

    Fixes broken spec for Types::Nullable
    No failure was raised if there was no exception. When I fixed this, I
    realized that our Nullable types defaults values.

--- spec/support/matchers/type/have_nullable_date_matcher.rb
+++ spec/support/matchers/type/have_nullable_date_matcher.rb
@@ -6,11 +6,53 @@ module Matchers
 
     class HaveNullableDateMatcher < HaveNullableMatcher
       def initialize( attribute, valid_value, invalid_value )
+        @attribute = attribute
+        @valid_value = valid_value
+        @invalid_value = invalid_value
         @expected_error = ArgumentError
         @expected_error_message = 'invalid date'
-
-        super( attribute, valid_value, invalid_value )
+        @failure_description = ''
       end
+
+      def matches?( klass )
+        @klass = klass
+
+        accepts_nil? && accepts_valid_value? && rejects_invalid_value?
+      end
+
+      def description
+        "have nullable attribute #{ @attribute.inspect }"
+      end
+
+      def failure_message
+        "Expected class to have nullable attribute #{ @attribute.inspect }" << @failure_description
+      end
+
+      private
+
+        def accepts_nil?
+          @klass.new(@attribute => nil)
+        end
+
+        def accepts_valid_value?
+          model = @klass.new(@attribute => @valid_value)
+          model.send(@attribute) == @valid_value
+        end
+
+        def rejects_invalid_value?
+          @klass.new(@attribute => @invalid_value)
+
+          @failure_description << " (Expected #{ @expected_error }, but got none)"
+          return false
+        rescue @expected_error => error
+          if error.message == @expected_error_message
+            return true
+          else
+            fail_message = "Expected error message to include #{ expected_message.inspect }, "\
+                           "but was #{ error.message.inspect }"
+            fail(fail_message)
+          end
+        end
     end
   end
 end
--- spec/support/matchers/type/have_nullable_matcher.rb
+++ spec/support/matchers/type/have_nullable_matcher.rb
@@ -1,23 +1,22 @@
 module Matchers
   module Type
-    def have_nullable( attribute, valid_value, invalid_value )
-      HaveNullableMatcher.new( attribute, valid_value, invalid_value )
+    def have_nullable( attribute, valid_value, invalid_value, default_value )
+      HaveNullableMatcher.new( attribute, valid_value, invalid_value, default_value )
     end
 
     class HaveNullableMatcher
-      def initialize( attribute, valid_value, invalid_value )
+      def initialize( attribute, valid_value, invalid_value, default_value )
         @attribute = attribute
         @valid_value = valid_value
         @invalid_value = invalid_value
-        @expected_error ||= Fortnox::API::InvalidAttributeValueError
-        @expected_error_message ||= "#{ @invalid_value.inspect } (#{ @invalid_value.class }) "\
-                                    "has invalid type for #{ @attribute.inspect }"
+        @default_value = default_value
+        @failure_description = ''
       end
 
       def matches?( klass )
         @klass = klass
 
-        accepts_nil? && accepts_valid_value? && rejects_invalid_value?
+        accepts_nil? && accepts_valid_value? && defaults_invalid_value?
       end
 
       def description
@@ -25,7 +24,7 @@ module Matchers
       end
 
       def failure_message
-        "Expected class to have nullable attribute #{ @attribute.inspect }"
+        "Expected class to have nullable attribute #{ @attribute.inspect }" << @failure_description
       end
 
       private
@@ -39,16 +38,14 @@ module Matchers
           model.send(@attribute) == @valid_value
         end
 
-        def rejects_invalid_value?
-          @klass.new(@attribute => @invalid_value)
-        rescue @expected_error => error
-          if error.message == @expected_error_message
-            return true
-          else
-            fail_message = "Expected error message to include #{ expected_message.inspect }, "\
-                           "but was #{ error.message.inspect }"
-            fail(fail_message)
-          end
+        def defaults_invalid_value?
+          model = @klass.new(@attribute => @invalid_value)
+          value = model.send(@attribute)
+          return true if value == @default_value
+
+          @failure_description << " (Expected #{ @invalid_value.inspect } to default to "\
+                                  "#{ @default_value.inspect }), but got #{ value.inspect }"
+          false
         end
     end
   end
