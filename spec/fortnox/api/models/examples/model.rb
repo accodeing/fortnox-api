71f0b92f9f97af64bd205f4cb01ec31ff339b64a
Hannes Elvemyr <git@elvemyrconsulting.se>
2016-10-27 09:25:00 +0200

    Fixes Rubocop violations

--- spec/fortnox/api/models/examples/model.rb
+++ spec/fortnox/api/models/examples/model.rb
@@ -1,7 +1,6 @@
-# rubocop:disable RSpec/DescribeClass
 shared_examples_for 'a model' do |valid_hash, unique_id_attribute, value|
   it 'can be initialized' do
-    expect { described_class.new(valid_hash) }.not_to raise_error
+    expect{ described_class.new(valid_hash) }.not_to raise_error
   end
 
   describe '.unique_id' do
@@ -12,4 +11,3 @@ shared_examples_for 'a model' do |valid_hash, unique_id_attribute, value|
     it{ expect(model.unique_id).to eq value }
   end
 end
-# rubocop:enable RSpec/DescribeClass
