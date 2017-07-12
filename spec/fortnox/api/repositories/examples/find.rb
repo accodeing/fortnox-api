7c68d819bc1ea4102977947425360d76ab45d3c8
Hannes Elvemyr <git@elvemyrconsulting.se>
2017-07-12 12:22:41 +0200

    Adds test for Repository .find failure

--- spec/fortnox/api/repositories/examples/find.rb
+++ spec/fortnox/api/repositories/examples/find.rb
@@ -1,15 +1,16 @@
 # rubocop:disable RSpec/DescribeClass
 shared_examples_for '.find' do |searched_entity_id|
   describe '.find by id' do
-    let( :find_id_1 ) do
+    let( :returned_object ) do
       VCR.use_cassette( "#{ vcr_dir }/find_id_1" ){ repository.find( searched_entity_id ) }
     end
 
-    describe 'returned object' do
-      subject(:returned_object){ find_id_1 }
-
-      it{ is_expected.to be_saved }
-      it{ is_expected.not_to be_new }
+    context 'when found' do
+      describe 'returned object' do
+        subject{ returned_object }
+        it{ is_expected.to be_saved }
+        it{ is_expected.not_to be_new }
+      end
 
       describe 'class' do
         subject{ returned_object.class }
@@ -20,6 +21,19 @@ shared_examples_for '.find' do |searched_entity_id|
         subject{ returned_object.unique_id}
         it { is_expected.to eq searched_entity_id }
       end
+    end
+
+    context 'when not found' do
+      subject{ find_failure }
+
+      let( :not_found_id ){ '123456789' }
+      let( :find_failure ) do
+        when_performing do
+          VCR.use_cassette( "#{ vcr_dir }/find_failure" ){ repository.find( not_found_id ) }
+        end
+      end
+
+      it{ is_expected.to raise_error( Fortnox::API::RemoteServerError, /Kan inte hitta /) }
     end
   end
 end
