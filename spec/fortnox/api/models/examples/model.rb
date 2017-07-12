1a1217dc2f027cfb44e19441dfe0cd6537d17795
Hannes Elvemyr <git@elvemyrconsulting.se>
2017-07-12 16:38:24 +0200

    Renames variable in spec example group

--- spec/fortnox/api/models/examples/model.rb
+++ spec/fortnox/api/models/examples/model.rb
@@ -1,13 +1,13 @@
-shared_examples_for 'a model' do |valid_hash, unique_id_attribute, value|
+shared_examples_for 'a model' do |valid_hash, unique_id_attribute, unique_id|
   it 'can be initialized' do
     expect{ described_class.new(valid_hash) }.not_to raise_error
   end
 
   describe '.unique_id' do
-    let( :model ){ described_class.new( valid_hash.merge({ unique_id_attribute => value })) }
+    let( :model ){ described_class.new( valid_hash.merge({ unique_id_attribute => unique_id })) }
 
-    before{ expect(model.send(unique_id_attribute)).to eq value }
+    before{ expect(model.send(unique_id_attribute)).to eq unique_id }
 
-    it{ expect(model.unique_id).to eq value }
+    it{ expect(model.unique_id).to eq unique_id }
   end
 end
