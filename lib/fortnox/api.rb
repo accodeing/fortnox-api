803c5f01fc8564b8896d3b4cf5b490fae9d2134c
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@my-codeworks.com>
2017-10-10 15:15:55 +0200

    WiP - Support multiple token stores

--- lib/fortnox/api.rb
+++ lib/fortnox/api.rb
@@ -1,6 +1,8 @@
 require "set"
 require "dotenv"
 require "dry-struct"
+require "fortnox/api/configuration"
+require "fortnox/api/circular_queue"
 require "fortnox/api/base"
 require "fortnox/api/version"
 require 'logger'
@@ -9,6 +11,18 @@ Dotenv.load unless ENV['RUBY_ENV'] == 'test'
 
 module Fortnox
   module API
+
+    include Configuration.with(
+      base_url: 'https://api.fortnox.se/3/',
+      client_secret: nil,
+      token_store: {},
+      debugging: false,
+      logger: ->{
+        logger = Logger.new(STDOUT)
+        logger.level = Logger::WARN
+        return logger
+      }.call,
+    )
 
     class Exception < StandardError
     end
@@ -22,17 +36,42 @@ module Fortnox
     class MissingAttributeError < Fortnox::API::Exception
     end
 
-    class << self
-      @debugging = false
-      @logger = Logger.new(STDOUT)
-      attr_accessor :debugging
-      attr_accessor :logger
-
-      @logger.level = Logger::WARN
+    class MissingConfiguration < Fortnox::API::Exception
     end
 
-    def self.get_access_token
-      Base.get_access_token
+    def check_access_tokens!( tokens )
+      if tokens == nil or tokens.length.zero?
+        fail MissingConfiguration, "You have not provided any access token(s) in the given store."
+      end
+    end
+
+    def load_access_tokens( store_name )
+      tokens = config.token_store[ store_name ]
+      check_access_tokens!( tokens )
+      tokens
+    end
+
+    def get_access_token( store_name )
+      @access_tokens ||= CircularQueue.new( *load_access_tokens( store_name ))
+      @access_tokens.next
+    end
+
+    def get_base_url
+      base_url = config.base_url
+      fail MissingConfiguration, 'You have to provide a base url.' unless base_url
+      base_url
+    end
+
+    def get_client_secret
+      client_secret = config.client_secret
+      fail MissingConfiguration, 'You have to provide your client secret.' unless client_secret
+      client_secret
+    end
+
+    def get_authorization_code
+      authorization_code = config.authorization_code
+      fail MissingConfiguration, 'You have to provide your authorization code.' unless authorization_code
+      authorization_code
     end
 
     Registry = Dry::Container.new
