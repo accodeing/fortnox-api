246d536fe03e353f9148d40a1a66223f8fe363d9
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@my-codeworks.com>
2018-02-23 13:25:59 +0100

    Fixes build errors

--- spec/fortnox/api/repositories/unit_spec.rb
+++ spec/fortnox/api/repositories/unit_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 require 'fortnox/api'
 require 'fortnox/api/mappers'
-require 'fortnox/api/repositories/customer'
+require 'fortnox/api/repositories/unit'
 require 'fortnox/api/repositories/examples/all'
-require 'fortnox/api/repositories/examples/find'
 require 'fortnox/api/repositories/examples/save'
 require 'fortnox/api/repositories/examples/save_with_specially_named_attribute'
 
 describe Fortnox::API::Repository::Unit, order: :defined, integration: true do
-  subject(:repository){ described_class.new }
+  include Helpers::Configuration
+  
+  before { set_api_test_configuration }
+
+  subject(:repository) { described_class.new }
 
   include_examples '.save',
                    :description,
