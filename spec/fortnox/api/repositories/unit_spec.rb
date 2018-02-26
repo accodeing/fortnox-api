a1550f4f700153da7a1c6c6366e5e6ad79a3584f
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@my-codeworks.com>
2018-02-26 12:02:25 +0100

    Fixes residual rubocop offences

--- spec/fortnox/api/repositories/unit_spec.rb
+++ spec/fortnox/api/repositories/unit_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'fortnox/api'
 require 'fortnox/api/mappers'
@@ -8,7 +10,7 @@ require 'fortnox/api/repositories/examples/save_with_specially_named_attribute'
 
 describe Fortnox::API::Repository::Unit, order: :defined, integration: true do
   include Helpers::Configuration
-  
+
   before { set_api_test_configuration }
 
   subject(:repository) { described_class.new }
