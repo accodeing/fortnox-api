90db3b187e329e4a53c2ef24f4d9dc2b5ce3ce2d
Mark <birmanmark@gmail.com>
2018-03-09 19:06:43 +0300

    Rename subject to circular_queue

--- spec/fortnox/api/circular_queue_spec.rb
+++ spec/fortnox/api/circular_queue_spec.rb
@@ -4,29 +4,32 @@ require 'spec_helper'
 require 'fortnox/api/circular_queue'
 
 RSpec.describe Fortnox::API::CircularQueue do
-  subject { described_class.new(*items) }
-
+  let(:circular_queue) { described_class.new(*items) }
   let(:items) { [rand, rand] }
 
   context 'when there are items to iterate' do
     it 'increments the @@next_index' do
-      subject.class.class_variable_set(:@@next_index, 0)
+      circular_queue.class.class_variable_set(:@@next_index, 0)
 
-      expect { subject.next }.to change { subject.class.class_variable_get(:@@next_index) }.to(1)
+      expect do
+        circular_queue.next
+      end.to change { circular_queue.class.class_variable_get(:@@next_index) }.to(1)
     end
   end
 
   context 'when the end of the queue is reached' do
     it 'resets the @@next_index' do
-      subject.class.class_variable_set(:@@next_index, items.size - 1)
+      circular_queue.class.class_variable_set(:@@next_index, items.size - 1)
 
-      expect { subject.next }.to change { subject.class.class_variable_get(:@@next_index) }.to(0)
+      expect do
+        circular_queue.next
+      end.to change { circular_queue.class.class_variable_get(:@@next_index) }.to(0)
     end
   end
 
   it 'returns the current value' do
-    index = subject.class.class_variable_get(:@@next_index)
+    index = circular_queue.class.class_variable_get(:@@next_index)
 
-    expect(subject.next).to eq(items[index])
+    expect(circular_queue.next).to eq(items[index])
   end
 end
