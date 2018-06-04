035e4f03d87dcfaa7beef129130e4b59349325dc
Samuel Nilsson <mail@samuelnilsson.me>
2018-06-04 20:50:10 +0200

    Add STUB to Metadata model

--- lib/fortnox/api/models/metadata.rb
+++ lib/fortnox/api/models/metadata.rb
@@ -6,7 +6,11 @@ module Fortnox
   module API
     module Model
       class Metadata < Model::Base
-        STUB = {}.freeze
+        STUB = {
+          current_page: '',
+          total_resources: '',
+          total_pages: ''
+        }.freeze
 
         attribute :current_page, Types::Required::Integer.is(:read_only)
         attribute :total_resources, Types::Required::Integer.is(:read_only)
