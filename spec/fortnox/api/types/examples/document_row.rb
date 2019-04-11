d7d3f1c8f951936af440e36d5c3dac7a8f093565
Hannes Elvemyr <hannes@accodeing.com>
2019-04-11 10:52:56 +0200

    Disables Rubocop rule on `it` block in spec
    In this block, it makes sense to allow one extra line (6 instead of 5).

--- spec/fortnox/api/types/examples/document_row.rb
+++ spec/fortnox/api/types/examples/document_row.rb
@@ -14,7 +14,7 @@ shared_examples_for 'DocumentRow' do |valid_hash|
 
   it { is_expected.to have_sized_float(:price, 0.0, 99_999_999_999.9, valid_hash) }
 
-  it do
+  it do # rubocop:disable RSpec/ExampleLength
     is_expected.to have_sized_float(
       :delivered_quantity,
       -9_999_999_999_999.9,
