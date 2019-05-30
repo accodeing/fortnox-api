f0ccdd49836b36881bd8d5f65b0daaa36c47d07e
Hannes Elvemyr <hannes@accodeing.com>
2019-05-30 10:32:54 +0200

    Simplifies logic in CanonicalNameSym

--- lib/fortnox/api/mappers/base/canonical_name_sym.rb
+++ lib/fortnox/api/mappers/base/canonical_name_sym.rb
@@ -13,11 +13,9 @@ module Fortnox
                     values.first.class
                   end
 
-          if RUBY_VERSION >= '2.4'
-            # Ruby 2.4 unifies Fixnum and Bignum into Integer
-            # Stringify to avoid warnings on newer Ruby versions
-            klass = Integer if %w[Bignum Fixnum].include?(klass.to_s)
-          end
+          # Ruby 2.4 unifies Fixnum and Bignum into Integer
+          # Stringify to avoid warnings on newer Ruby versions
+          klass = Integer if RUBY_VERSION >= '2.4' && %w[Bignum Fixnum].include?(klass.to_s)
 
           klass.name.split('::').last.downcase.to_sym
         end
