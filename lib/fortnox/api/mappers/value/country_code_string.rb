d3f9ecb8cbbd5e401d21916ea622ca6e2dfb9256
Hannes Elvemyr <hannes@accodeing.com>
2019-05-30 10:05:26 +0200

    Refactors CountryCode mapper
    - Renames it to CountryCodeString
    - Adds proper registration with canonical_name_sym

---
+++ lib/fortnox/api/mappers/value/country_code_string.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'fortnox/api/mappers/base/canonical_name_sym'
+
+module Fortnox
+  module API
+    module Mapper
+      class CountryCodeString
+        extend CanonicalNameSym
+
+        CountryCodeMapper = lambda do |code|
+          # Fortnox only supports Swedish country name for Sweden
+          next 'Sverige' if code == 'SE'
+
+          ::ISO3166::Country[code].translations['en']
+        end
+
+        Registry.register(self.canonical_name_sym, CountryCodeMapper)
+      end
+    end
+  end
+end
