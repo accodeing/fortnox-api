6ecfbce3339b28e32548102ad44326442e1a1bee
Hannes Elvemyr <hannes@accodeing.com>
2019-09-12 07:19:33 +0200

    Rearranges spec description for AccountNumber

--- spec/fortnox/api/types/account_number_spec.rb
+++ spec/fortnox/api/types/account_number_spec.rb
@@ -7,25 +7,27 @@ require 'fortnox/api/types/examples/types'
 describe Fortnox::API::Types do
   let(:klass) { Fortnox::API::Types::AccountNumber }
 
-  context 'when AccountNumber created with nil' do
-    subject { klass[nil] }
+  describe 'AccountNumber' do
+    context 'when created with nil' do
+      subject { klass[nil] }
 
-    it { is_expected.to be_nil }
-  end
+      it { is_expected.to be_nil }
+    end
 
-  context 'when AccountNumber created with empty string' do
-    include_examples 'raises ConstraintError', ''
-  end
+    context 'when created with empty string' do
+      include_examples 'raises ConstraintError', ''
+    end
 
-  context 'when AccountNumber created with valid number' do
-    include_examples 'equals input', 1234
-  end
+    context 'when created with valid number' do
+      include_examples 'equals input', 1234
+    end
 
-  context 'when AccountNumber created with a too large number' do
-    include_examples 'raises ConstraintError', 10_000
-  end
+    context 'when created with a too large number' do
+      include_examples 'raises ConstraintError', 10_000
+    end
 
-  context 'when AccountNumber created with a negative number' do
-    include_examples 'raises ConstraintError', -1
+    context 'when created with a negative number' do
+      include_examples 'raises ConstraintError', -1
+    end
   end
 end
