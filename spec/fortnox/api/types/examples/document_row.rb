c7e4ab5a2f128ce5faf724598d796423ec470ca9
Hannes Elvemyr <hannes@accodeing.com>
2021-10-29 17:22:14 +0200

    Fixes #201 adds support for negative price in rows

--- spec/fortnox/api/types/examples/document_row.rb
+++ spec/fortnox/api/types/examples/document_row.rb
@@ -12,7 +12,7 @@ shared_examples_for 'DocumentRow' do |valid_hash|
 
   it { is_expected.to have_housework_type(:housework_type, valid_hash) }
 
-  it { is_expected.to have_sized_float(:price, 0.0, 99_999_999_999.9, valid_hash) }
+  it { is_expected.to have_sized_float(:price, -9_999_999_999, 99_999_999_999.9, valid_hash) }
 
   it do # rubocop:disable RSpec/ExampleLength
     is_expected.to have_sized_float(
