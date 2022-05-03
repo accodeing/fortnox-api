ec32c34ed7ffb9e96149af43cb603931b7ec3cac
Hannes Elvemyr <hannes@accodeing.com>
2022-05-03 22:16:28 +0200

    Fixes Rubocop violations

--- spec/fortnox/api/types/sales_account_spec.rb
+++ spec/fortnox/api/types/sales_account_spec.rb
@@ -20,16 +20,16 @@ describe Fortnox::API::Types do
       it { is_expected.to be_nil }
     end
 
-    [-12345, -1234, -123, -1, 0, 1, 12, 123, 1234.0, 12345].each do |invalid_number|
+    [-12_345, -1_234, -123, -1, 0, 1, 12, 123, 1_234.0, 12_345].each do |invalid_number|
       context "with #{invalid_number}" do
-        it "raises ConstraintError" do
+        it 'raises ConstraintError' do
           expect { klass[invalid_number] }.to raise_error(Dry::Types::ConstraintError)
         end
       end
     end
 
     context 'with 4 digits' do
-      subject { klass[1234] }
+      subject { klass[1_234] }
 
       it { is_expected.to eq('1234') }
     end
