bbcc0455c01b0d29b39797052697269fd7950b40
Hannes Elvemyr <hannes@accodeing.com>
2022-06-02 12:47:17 +0200

    Fixes broken VCR setup

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -6,6 +6,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
   c.filter_sensitive_data('<AUTHORIZATION>') do |interaction|
-    interaction.request.headers['Authorization'].first
+    interaction.request.headers['Authorization']&.first
   end
 end
