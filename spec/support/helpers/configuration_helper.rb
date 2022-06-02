55e63b35280f65e0bd45c4bc49003176c0b10941
Hannes Elvemyr <hannes@accodeing.com>
2022-06-02 12:47:02 +0200

    Updates configuration helper for specs

--- spec/support/helpers/configuration_helper.rb
+++ spec/support/helpers/configuration_helper.rb
@@ -1,14 +1,37 @@
 # frozen_string_literal: true
 
 require 'dotenv'
+Dotenv.load('.env.test')
+
+class FixedTokenStore
+  def access_token
+    ENV.fetch('FORTNOX_API_ACCESS_TOKEN')
+  end
+
+  def refresh_token
+    raise Exception,
+          "Something went wrong, #refresh_token should not be called during this test." \
+          "Check that the access token is valid."
+  end
+
+  def access_token=(token);
+    raise Exception,
+          "Something went wrong, #access_token= should not be called during this test." \
+          "Check that the access token is valid."
+  end
+
+  def refresh_token=(token)
+    raise Exception,
+          "Something went wrong, #refresh_token= should not be called during this test." \
+          "Check that the access token is valid."
+  end
+end
 
 module Helpers
   module Configuration
+
     def set_api_test_configuration
-      Fortnox::API.configure do |config|
-        config.client_secret = ENV['FORTNOX_API_CLIENT_SECRET']
-        config.access_tokens = [ENV['FORTNOX_API_ACCESS_TOKEN']]
-      end
+      Fortnox::API.configure { |config| config.storage = FixedTokenStore.new }
     end
   end
 end
