957f6a28e83c332a2732d418a01e0317bc2c13f0
Hannes Elvemyr <hannes@accodeing.com>
2022-06-07 09:32:31 +0200

    Simplifies mocked access token for tests

--- spec/support/helpers/configuration_helper.rb
+++ spec/support/helpers/configuration_helper.rb
@@ -7,17 +7,6 @@ Dotenv.load('.env.test')
 
 class Storage
   def access_token
-    if ENV.fetch('MOCK_VALID_ACCESS_TOKEN') == 'true'
-      # Mock long lived token to avoid using a real one from Fortnox
-      # 1906691557 = Mon Jun 03 2030 04:32:37 GMT+0000
-      return JWT.encode({ exp: 1_906_691_557 }, nil, 'none')
-    end
-
-    # When you want to use a real token, for instance to rerecord VCR
-    # cassettes or when adding new integration specs, set `MOCK_VALID_ACCESS_TOKEN`
-    # to false and provide a valid access token in `.env`.
-    # See `bin/get_tokens` to issue valid tokens.
-    Dotenv.load('.env')
     ENV.fetch('FORTNOX_API_ACCESS_TOKEN')
   end
 
