346fb977a7985263c2f566b01a6585ee3fe3a3b4
Hannes Elvemyr <hannes@accodeing.com>
2023-01-30 11:41:20 +0100

    Fixes broken to_json spec

--- spec/fortnox/api/mappers/base/to_json_spec.rb
+++ spec/fortnox/api/mappers/base/to_json_spec.rb
@@ -13,65 +13,67 @@ describe Fortnox::API::Mapper::ToJSON do
     it 'should be tested'
   end
 
-  # include_context 'with JSON conversion'
+  include_context 'with JSON conversion'
 
-  # before do
-  #   module Test
-  #     class BaseMapper
-  #       include Fortnox::API::Mapper::ToJSON
-  #     end
-  #   end
-  # end
+  before do
+    Test::BaseMapper.class_eval do
+      include Fortnox::API::Mapper::ToJSON # rubocop:disable RSpec/DescribedClass
+    end
 
-  # let( :mapper ){ Test::ProductMapper.new }
+    # Are these needed?
+    register_mapper(:categories, Test::CategoryMapper)
+    register_mapper(:designer, Test::ProductDesignerMapper)
+  end
 
-  # describe 'entity_to_hash' do
-  #   let( :category1 ){ Test::Category.new( name: 'Cars', id: '1' ) }
-  #   let( :category2 ){ Test::Category.new( name: 'Fast Cars', id: '2' ) }
-  #   let( :product_designer ){ Test::ProductDesigner.new( name: 'John Najjar', id: '23' ) }
-  #   let( :product ) do
-  #     Test::Product.new( url: 'someurl@example.com',
-  #                        name: 'Ford Mustang',
-  #                        vat: 30000.0 ,
-  #                        categories: [category1, category2],
-  #                        designer: product_designer )
-  #   end
-  #   let( :keys_to_filter ){ [:url] }
-  #   let( :returned_hash ){ mapper.entity_to_hash( product, keys_to_filter ) }
-  #   let( :inner_hash ){ returned_hash['Product'] }
+  let(:mapper){ Test::ProductMapper.new }
 
-  #   it 'includes JSON entity wrapper' do
-  #     expect( returned_hash ).to have_key( 'Product' )
-  #   end
+  describe 'entity_to_hash' do
+    let(:category1){ Test::Category.new( name: 'Cars', id: '1' ) }
+    let(:category2){ Test::Category.new( name: 'Fast Cars', id: '2' ) }
+    let(:product_designer){ Test::ProductDesigner.new( name: 'John Najjar', id: '23' ) }
+    let(:product) do
+      Test::Product.new(url: 'someurl@example.com',
+                        name: 'Ford Mustang',
+                        vat: 30000.0 ,
+                        categories: [category1, category2],
+                        designer: product_designer)
+    end
+    let(:keys_to_filter){ [:url] }
+    let(:returned_hash){ mapper.entity_to_hash( product, keys_to_filter ) }
+    let(:inner_hash){ returned_hash['Product'] }
 
-  #   context 'with keys without mapping' do
-  #     specify 'converts correctly' do
-  #       expect( inner_hash['Name'] ).to eq( 'Ford Mustang' )
-  #     end
-  #   end
+    it 'includes JSON entity wrapper' do
+      expect( returned_hash ).to have_key( 'Product' )
+    end
 
-  #   context 'with keys to filter' do
-  #     specify 'filteres out those keys' do
-  #       expect( inner_hash ).not_to have_key( '@url' )
-  #     end
-  #   end
+    describe 'keys without mapping' do
+      specify 'converts correctly' do
+        expect( inner_hash['Name'] ).to eq( 'Ford Mustang' )
+      end
+    end
 
-  #   context 'with nested models' do
-  #     let( :expected_nested_model_hash ) do
-  #       [{"Name" => "Cars", "ID" => "1"}, {"Name" => "Fast Cars", "ID" => "2"}]
-  #     end
+    describe 'keys to filter' do
+      specify 'filteres out those keys' do
+        expect( inner_hash ).not_to have_key( '@url' )
+      end
+    end
 
-  #     specify 'are converted correctly' do
-  #       expect( inner_hash['Categories'] ).to eq( expected_nested_model_hash )
-  #     end
-  #   end
+    describe 'nested models' do
+      let(:expected_nested_model_hash ) do
+        [{"Name" => "Cars", "ID" => "1"}, {"Name" => "Fast Cars", "ID" => "2"}]
+      end
 
-  #   context 'with nested model' do
-  #     let( :expected_nested_model_hash ){ { 'Name' => 'John Najjar', 'ID' => '23' } }
+      specify 'are converted correctly' do
+        expect( inner_hash['Categories'] ).to eq( expected_nested_model_hash )
+      end
+    end
 
-  #     specify 'is converted correctly' do
-  #       expect( inner_hash['Designer'] ).to eq( expected_nested_model_hash )
-  #     end
-  #   end
-  # end
+    describe 'nested model' do
+      let(:expected_nested_model_hash ){ { 'Name' => 'John Najjar', 'ID' => '23' } }
+
+      specify 'is converted correctly' do
+        expect( inner_hash['Designer'] ).to eq( expected_nested_model_hash )
+      end
+    end
+  end
 end
