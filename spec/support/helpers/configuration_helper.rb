07cfb24927c4bc0b3372568d37d9d5d94b733d99
Hannes Elvemyr <hannes@accodeing.com>
2023-01-31 12:22:07 +0100

    Cleans Configuration Helper

--- spec/support/helpers/configuration_helper.rb
+++ spec/support/helpers/configuration_helper.rb
@@ -8,30 +8,6 @@ Dotenv.load(DOTENV_FILE_NAME)
 
 DEBUG = ENV.fetch('DEBUG', false)
 
-class TokenStore
-  def access_token
-    ENV.fetch('FORTNOX_API_ACCESS_TOKEN')
-  end
-
-  def refresh_token
-    raise StandardError,
-          'Something went wrong, #refresh_token should not be called during this test. ' \
-          'Verify that the access token is valid.'
-  end
-
-  def access_token=(token)
-    raise StandardError,
-          'Something went wrong, #access_token= should not be called during this test. ' \
-          'Verify that the access token is valid.'
-  end
-
-  def refresh_token=(token)
-    raise StandardError,
-          'Something went wrong, #refresh_token= should not be called during this test. ' \
-          'Verify that the access token is valid.'
-  end
-end
-
 module Helpers
   module Configuration
     def set_api_test_configuration
