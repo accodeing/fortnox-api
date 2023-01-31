a4bd621090d3f6c9cc3ef04a2759a7e304713f29
Hannes Elvemyr <hannes@accodeing.com>
2023-01-31 09:52:46 +0100

    Adds VCR config for ignoring tokens in requests

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -5,10 +5,20 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
-  c.ignore_request do |request|
-    request.uri == 'https://apps.fortnox.se/oauth-v1/token' if ENV.fetch('REFRESH_TOKENS')
-  end
   c.filter_sensitive_data('<AUTHORIZATION>') do |interaction|
     interaction.request.headers['Authorization']&.first
+  end
+  c.filter_sensitive_data('<REFRESH_TOKEN>') do |interaction|
+    interaction.request.body.split('&refresh_token=').last
+  end
+  c.filter_sensitive_data('<ACCESS_TOKEN>') do |interaction|
+    body = interaction.response.body
+    access_token = /"access_token":"[^"]*"/.match(body)
+    access_token ? access_token.to_s.split('"access_token":"').last.chop : ''
+  end
+  c.filter_sensitive_data('<REFRESH_TOKEN>') do |interaction|
+    body = interaction.response.body
+    refresh_token = /"refresh_token":"[^"]*"/.match(body)
+    refresh_token ? refresh_token.to_s.split('"refresh_token":"').last.chop : ''
   end
 end
