64c891ea72b9ea50830dbcee554aa13e8703b1e4
Hannes Elvemyr <hannes@accodeing.com>
2026-04-29 14:24:20 +0200

    Clean attribute comments

--- lib/fortnox/resources/customer.rb
+++ lib/fortnox/resources/customer.rb
@@ -10,181 +10,181 @@ module Fortnox
       collection_wrapper 'Customers'
     end
 
-    # @url Direct URL to the record.
+    # Direct URL to the record.
     attr :url <=> '@url', String.optional, :read_only
 
-    # Address1 First address of the customer
+    # First address of the customer
     attr :address1, Sized::String[1024]
 
-    # Address2 Second address of the customer
+    # Second address of the customer
     attr :address2, Sized::String[1024]
 
-    # City City of the customer
+    # City of the customer
     attr :city, Sized::String[1024]
 
-    # Country Country of the customer. Read-only.
+    # Country of the customer
     attr :country, String.optional, :read_only
 
-    # Comments Comments.
+    # Comments
     attr :comments, Sized::String[1024]
 
-    # Currency Currency of the customer, 3 letters
+    # Currency of the customer
     attr :currency, Currencies
 
-    # CostCenter Cost center of the customer
+    # Cost center of the customer
     attr :cost_center, String.optional
 
-    # CountryCode Country code of the customer, 2 letters
+    # Country code of the customer
     attr :country_code, Sized::String[2]
 
-    # CustomerNumber Customer number
+    # Customer number
     key :customer_number, Sized::String[1024]
 
-    # DefaultDeliveryTypes
+    # Default delivery types
     attr :default_delivery_types, Structs::DefaultDeliveryTypes, Mappers::Struct.for(Structs::DefaultDeliveryTypes)
 
-    # DefaultTemplates
+    # Default templates
     attr :default_templates, Structs::DefaultTemplates, Mappers::Struct.for(Structs::DefaultTemplates)
 
-    # DeliveryAddress1 First delivery address of the customer
+    # First delivery address of the customer
     attr :delivery_address1, Sized::String[1024]
 
-    # DeliveryAddress2 Second delivery address of the customer
+    # Second delivery address of the customer
     attr :delivery_address2, Sized::String[1024]
 
-    # DeliveryCity Delivery city of the customer
+    # Delivery city of the customer
     attr :delivery_city, Sized::String[1024]
 
-    # DeliveryCountry Delivery country of the customer. Read-only.
+    # Delivery country of the customer
     attr :delivery_country, String.optional, :read_only
 
-    # DeliveryCountryCode Delivery country code of the customer, 2 letters
+    # Delivery country code of the customer
     attr :delivery_country_code, Sized::String[2]
 
-    # DeliveryFax Delivery fax number of the customer
+    # Delivery fax number of the customer
     attr :delivery_fax, Sized::String[1024]
 
-    # DeliveryName Delivery name of the customer
+    # Delivery name of the customer
     attr :delivery_name, Sized::String[1024]
 
-    # DeliveryPhone1 First delivery phone number of the customer
+    # First delivery phone number of the customer
     attr :delivery_phone1, Sized::String[1024]
 
-    # DeliveryPhone2 Second delivery phone number of the customer
+    # Second delivery phone number of the customer
     attr :delivery_phone2, Sized::String[1024]
 
-    # DeliveryZipCode Delivery zip code of the customer.
+    # Delivery zip code of the customer
     attr :delivery_zip_code, Sized::String[1024]
 
-    # Email Email address of the customer. 1024 characters
+    # Email address of the customer
     attr :email, Email
 
-    # EmailInvoice Invoice email address of the customer. 1024 characters
+    # Invoice email address of the customer
     attr :email_invoice, Email
 
-    # EmailInvoiceBCC Invoice BCC email address of the customer. 1024 characters
+    # Invoice BCC email address of the customer
     attr :email_invoice_bcc <=> 'EmailInvoiceBCC', Email
 
-    # EmailInvoiceCC Invoice CC email address of the customer. 1024 characters
+    # Invoice CC email address of the customer
     attr :email_invoice_cc <=> 'EmailInvoiceCC', Email
 
-    # EmailOffer Offer email address of the customer. 1024 characters
+    # Offer email address of the customer
     attr :email_offer, Email
 
-    # EmailOfferBCC Offer BCC email address of the customer. 1024 characters
+    # Offer BCC email address of the customer
     attr :email_offer_bcc <=> 'EmailOfferBCC', Email
 
-    # EmailOfferCC Offer CC email address of the customer. 1024 characters
+    # Offer CC email address of the customer
     attr :email_offer_cc <=> 'EmailOfferCC', Email
 
-    # EmailOrder Order email address of the customer. 1024 characters
+    # Order email address of the customer
     attr :email_order, Email
 
-    # EmailOrderBCC Order BCC email address of the customer. 1024 characters
+    # Order BCC email address of the customer
     attr :email_order_bcc <=> 'EmailOrderBCC', Email
 
-    # EmailOrderCC Order CC email address of the customer. 1024 characters
+    # Order CC email address of the customer
     attr :email_order_cc <=> 'EmailOrderCC', Email
 
-    # Fax Fax number of the customer
+    # Fax number of the customer
     attr :fax, Sized::String[1024]
 
-    # InvoiceAdministrationFee Invoice administration fee of the customer, 12 digits (incl. decimals).
+    # Invoice administration fee, 12 digits (incl. decimals)
     attr :invoice_administration_fee, Sized::Float[0.0, 99_999_999_999.9]
 
-    # InvoiceDiscount Invoice discount of the customer, 12 digits (incl. decimals)
+    # Invoice discount, 12 digits (incl. decimals)
     attr :invoice_discount, Sized::Float[0.0, 99_999_999_999.9]
 
-    # InvoiceFreight Invoice freight fee of the customer, 12 digits (incl. decimals)
+    # Invoice freight fee, 12 digits (incl. decimals)
     attr :invoice_freight, Sized::Float[0.0, 99_999_999_999.9]
 
-    # InvoiceRemark Invoice remark of the customer
+    # Invoice remark
     attr :invoice_remark, Sized::String[1024]
 
-    # Name Name of the customer
+    # Name of the customer
     attr :name, Sized::String[1024], :required
 
-    # OrganisationNumber Organisation number of the customer
+    # Organisation number of the customer
     attr :organisation_number, Sized::String[30]
 
-    # OurReference Our reference of the customer
+    # Our reference
     attr :our_reference, Sized::String[50]
 
-    # Phone1 First phone number of the customer
+    # First phone number of the customer
     attr :phone1, Sized::String[1024]
 
-    # Phone2 Second phone number of the customer
+    # Second phone number of the customer
     attr :phone2, Sized::String[1024]
 
-    # PriceList Price list of the customer
+    # Price list of the customer
     attr :price_list, String.optional
 
-    # Project Project of the customer
+    # Project of the customer
     attr :project, String.optional
 
-    # SalesAccount Sales account of the customer
+    # Sales account of the customer
     attr :sales_account, AccountNumber
 
-    # ShowPriceVATIncluded Show prices with VAT included or not
+    # Show prices with VAT included or not
     attr :show_price_vat_included <=> 'ShowPriceVATIncluded', Boolean.optional
 
-    # TermsOfDelivery Terms of delivery code of the customer
+    # Terms of delivery code
     attr :terms_of_delivery, String.optional
 
-    # TermsOfPayment Terms of payment code of the customer
+    # Terms of payment code
     attr :terms_of_payment, String.optional
 
-    # Type Customer type, PRIVATE / COMPANY
+    # Customer type
     attr :type, CustomerTypes
 
-    # VATNumber VAT number of the customer
+    # VAT number of the customer
     attr :vat_number <=> 'VATNumber', String.optional
 
-    # VATType VAT type of the customer
+    # VAT type of the customer
     attr :vat_type <=> 'VATType', VATTypes
 
-    # VisitingAddress Visit address of the customer
+    # Visit address of the customer
     attr :visiting_address, Sized::String[128]
 
-    # VisitingCity Visit city of the customer
+    # Visit city of the customer
     attr :visiting_city, Sized::String[128]
 
-    # VisitingCountry Visit country of the customer, read-only
+    # Visit country of the customer
     attr :visiting_country, String.optional, :read_only
 
-    # VisitingCountryCode Code of the visiting country for the customer, 2 letters
+    # Visiting country code
     attr :visiting_country_code, Sized::String[2]
 
-    # VisitingZipCode Visit zip code of the customer
+    # Visit zip code of the customer
     attr :visiting_zip_code, Sized::String[10]
 
-    # WayOfDelivery Way of delivery code of the customer
+    # Way of delivery code
     attr :way_of_delivery, String.optional
 
-    # YourReference Your reference of the customer
+    # Your reference
     attr :your_reference, Sized::String[50]
 
-    # ZipCode Zip code of the customer
+    # Zip code of the customer
     attr :zip_code, Sized::String[10]
 
     # Active If the customer is active.
